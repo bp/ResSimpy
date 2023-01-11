f46ced68841d342c86d6a0bac76ba2e64ffa9284
Hall <Jonathan.Hall@uk.bp.com>
2023-01-11 10:42:34 +0000

    adds documentation and refactors variable name in replace_value function

--- ResSimpy/Nexus/nexus_file_operations.py
+++ ResSimpy/Nexus/nexus_file_operations.py
@@ -240,7 +240,15 @@ def load_token_value_if_present(token: str, modifier: str, token_property: Varia
                 token_property.modifier = modifier
 
 
-def get_simulation_time(line):
+def get_simulation_time(line: str) -> str:
+    """searches for the simulation time in a line
+
+    Args:
+        line (str): line to search for the simulation time
+
+    Returns:
+        str: value found after TIME card in a line
+    """    
     value_found = False
     value = ''
     line_string = line
@@ -259,28 +267,39 @@ def get_simulation_time(line):
     return value
 
 
-def replace_value(file, old_property, new_property, string_name):
-    """ Replace the value and token + modifier with the new values """
-    for line in file:
-        old_token_modifier = f"{string_name} {old_property.modifier}"
-        new_token_modifier = f"{string_name} {new_property.modifier}"
+def replace_value(file_as_list: list[str], old_property: VariableEntry, new_property: VariableEntry, 
+        token_name: str) -> None:
+    """Replace the value and token + modifier with the new values
+
+    Args:
+        file_as_list (list[str]): a list of strings containing each line of the file as a new entry
+        old_property (VariableEntry): property found in the original file to be replaced 
+        new_property (VariableEntry): new property to replace the old property with
+        token_name (str): name of the token being replaced
+    Returns:
+        None: modifies the file_as_list with the new property
+    """
+
+    for line in file_as_list:
+        old_token_modifier = f"{token_name} {old_property.modifier}"
+        new_token_modifier = f"{token_name} {new_property.modifier}"
         ignore_values = ['INCLUDE'] if old_property.modifier == 'VALUE' else []
         if old_token_modifier in line:
             # If we are replacing a mult, replace the first value with a blank
             if old_property.modifier == 'MULT':
                 dummy_value = VariableEntry('MULT', '')
-                get_token_value(old_token_modifier, line, file, ignore_values=ignore_values,
+                get_token_value(old_token_modifier, line, file_as_list, ignore_values=ignore_values,
                                 replace_with=dummy_value)
 
-            get_token_value(old_token_modifier, line, file, ignore_values=ignore_values,
+            get_token_value(old_token_modifier, line, file_as_list, ignore_values=ignore_values,
                             replace_with=new_property)
 
             new_line = line.replace(old_token_modifier, new_token_modifier, 1)
-            line_index = file.index(line)
-            file[line_index] = new_line
+            line_index = file_as_list.index(line)
+            file_as_list[line_index] = new_line
 
 
-def clean_up_string(value: str):
+def clean_up_string(value: str) -> str:
     """ Removes unwanted characters from a string """
     value = value.replace("\n", "")
     value = value.replace("!", "")
