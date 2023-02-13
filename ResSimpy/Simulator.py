52f79fcb4a13c799697ca461131cb376d86eb429
Jonny Hall <121860375+jonnyh404@users.noreply.github.com>
2023-02-13 15:49:24 +0000

    Well inheritance update (#19)
    * Checking in the code after demo session with three working test cases.
    
    * Checking in changes to dataclasses.
    
    * Fixing type issues.
    
    * Adding more test cases and a concept of units.
    
    * Slight refactor of load_wells, as well as creating a new nexus_token_found() function to check if a line contains a valid Nexus token.
    
    * Adding to Nexus constants.
    
    * Automatically loading in the wellspec file when loading in a Nexus model.
    
    * Adding in a test to confirm that the simulator reads in the wells file when it is found in the FCS file.
    
    * Fixing static analysis issues.
    
    * More Mypy issues.
    
    * Correcting a couple of tests.
    
    * use check_token in fcs load
    
    * move abs path function to nexus file ops and make fcs_reader use this function instead
    
    * allows users to load multiple wells in a single wells file
    
    * adds ability to load multiple wells at multiple dates
    
    * adds all well completion headers and values to NexusCompletion tables
    
    * converts NA to None within the wellspec tables
    
    * adds partial perf to NexusCompletion and fixes tests for multiple wells
    
    * adds docstrings to the load_wells functions
    
    * adds to_dict method for Completions and subclass NexusCompletions
    
    * adds to df method for nexuswells
    
    * removes redundant comments
    
    * refactors the casting functions for completions attribute assignment
    
    * adds the ability to filter for SET when getting wells files and test coverage of expected behaviour
    
    * adds the ability to filter for SET when getting wells files and test coverage of expected behaviour
    
    * adds comments for load wells and reduces indents
    
    * adds ignoring pandas import to mypy config
    
    * adds inheritence of the Wells class to Simulator Base Class, moves get wells path to a separate function out of the fcs path
    
    * adds inheritence of the Wells class to Simulator Base Class, moves get wells path to a separate function out of the fcs path
    
    * adds a way of protecting the Wells attribute from being written to with a non Wells class
    
    * fix duplicated if statement in merge
    
    * removing duplicate comments and extra return type hints
    
    ---------
    
    Co-authored-by: Chris Flynn <Chris@Flynnsoftware.com>

--- ResSimpy/Simulator.py
+++ ResSimpy/Simulator.py
@@ -2,13 +2,14 @@
 
 from abc import ABC, abstractmethod
 from typing import Optional
+from ResSimpy.Wells import Wells
 
 
 class Simulator(ABC):
 
     def __init__(self, start_date=''):
         self.__start_date = start_date
-
+        self.__Wells: Wells = Wells()
     """Class Properties"""
     @property
     def start_date(self) -> str:
@@ -16,6 +17,16 @@ class Simulator(ABC):
 
     def start_date_set(self, value) -> None:
         self.__start_date = value
+
+    @property
+    def Wells(self) -> Wells:
+        return self.__Wells
+
+    @Wells.setter
+    def Wells(self, cls):
+        if not isinstance(cls, Wells):
+            raise ValueError(f"Wells must take a valid Wells type. Instead got provided class of {type(cls)}")
+        self.__Wells = cls
 
     """ Class Methods """
     @staticmethod
