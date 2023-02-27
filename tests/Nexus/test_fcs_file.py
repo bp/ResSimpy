09b82191ae286699128ee4c2397994a2db44b25a
Jonny Hall <jonathan.hall@uk.bp.com>
2023-02-27 13:33:40 +0000

    fix to tests with spawning of empty list

--- tests/Nexus/test_fcs_file.py
+++ tests/Nexus/test_fcs_file.py
@@ -34,7 +34,7 @@ GRID_FILES
                                          'GRID_FILES', '	 STRUCTURED_GRID ', expected_structured_grid_file, '',
                                          '	 OPTIONS ', expected_options_file, '', ],
                                      structured_grid_file=expected_structured_grid_file,
-                                     options_file=expected_options_file,
+                                     options_file=expected_options_file, includes=[],
                                      )
 
     # Act
@@ -81,6 +81,7 @@ def test_fcs_file_multiple_methods(mocker):
             '	 EQUIL Method 2 ', expected_equil_2, '', '	 EQUIL Method 3 ',
             expected_equil_3, ''],
         equil_files={1: expected_equil_1, 2: expected_equil_2, 3: expected_equil_3},
+        includes=[],
     )
     # Act
     result = FcsNexusFile.generate_fcs_structure(fcs_path)
