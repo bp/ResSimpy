221580f363c341cd0588683db3842824545236d0
Jonny Hall <jonathan.hall@uk.bp.com>
2023-02-27 17:02:16 +0000

    use fcsfile class in the nexussimulator scripts

--- ResSimpy/Nexus/DataModels/FcsFile.py
+++ ResSimpy/Nexus/DataModels/FcsFile.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from dataclasses import Field, dataclass, field
 import os
 import warnings
@@ -19,7 +20,7 @@ class FcsNexusFile(NexusFile):
     override_file: Optional[NexusFile] = None
     eos_default_file: Optional[NexusFile] = None
     well_files: Optional[dict[int, NexusFile]] = field(default_factory=get_empty_dict_int_nexus_file)
-    surface_file: Optional[dict[int, NexusFile]] = field(default_factory=get_empty_dict_int_nexus_file)
+    surface_files: Optional[dict[int, NexusFile]] = field(default_factory=get_empty_dict_int_nexus_file)
     rock_files: Optional[dict[int, NexusFile]] = field(default_factory=get_empty_dict_int_nexus_file)
     relperm_files: Optional[dict[int, NexusFile]] = field(default_factory=get_empty_dict_int_nexus_file)
     pvt_files: Optional[dict[int, NexusFile]] = field(default_factory=get_empty_dict_int_nexus_file)
@@ -81,7 +82,7 @@ class FcsNexusFile(NexusFile):
         self.runcontrol_file = runcontrol_file
         self.override_file = override_file
         self.eos_default_file = eos_default_file
-        self.surface_file = surface_files
+        self.surface_files = surface_files
         self.well_files = well_files
         self.rock_files = rock_files
         self.relperm_files = relperm_files
@@ -106,7 +107,7 @@ class FcsNexusFile(NexusFile):
                          file_content_as_list=file_content_as_list)
 
     @classmethod
-    def generate_fcs_structure(cls, fcs_file_path: str, recursive: bool = True):
+    def generate_fcs_structure(cls, fcs_file_path: str, recursive: bool = True) -> FcsNexusFile:
         fcs_file = cls(location=fcs_file_path)
         fcs_file.includes_objects = get_empty_list_nexus_file()
         fcs_file.file_content_as_list = get_empty_list_str_nexus_file()
@@ -150,13 +151,13 @@ class FcsNexusFile(NexusFile):
             fcs_file_path, origin=None).get_flat_list_str_file()
         if flat_fcs_file_content is None:
             raise ValueError(f'FCS file not found, no content for {fcs_file_path=}')
-        # for key, item in fcs_keyword_map_single.items():
         for i, line in enumerate(flat_fcs_file_content):
             if nfo.nexus_token_found(line, valid_list=FCS_KEYWORDS):
                 key = nfo.get_next_value(start_line_index=i, file_as_list=flat_fcs_file_content, search_string=line)
                 if key is None:
                     warnings.warn(f'get next value failed to find a suitable token in {line}')
                     continue
+                key = key.upper()
                 value = nfo.get_token_value(key, line, flat_fcs_file_content[i::])
                 if value is None:
                     warnings.warn(f'No value found for {key}, skipping file')
@@ -170,7 +171,7 @@ class FcsNexusFile(NexusFile):
                     )
                     sub_file_path = nfo.get_full_file_path(value, fcs_file_path)
                     nexus_file = NexusFile.generate_file_include_structure(
-                        sub_file_path, origin=fcs_file_path)
+                        sub_file_path, origin=fcs_file_path, recursive=recursive)
                     fcs_property = getattr(fcs_file, fcs_keyword_map_multi[key])
                     if isinstance(fcs_property, Field):
                         fcs_property = get_empty_dict_int_nexus_file()
@@ -185,7 +186,7 @@ class FcsNexusFile(NexusFile):
                 elif key in fcs_keyword_map_single:
                     sub_file_path = nfo.get_full_file_path(value, fcs_file_path)
                     nexus_file = NexusFile.generate_file_include_structure(
-                        sub_file_path, origin=fcs_file_path)
+                        sub_file_path, origin=fcs_file_path, recursive=recursive)
                     setattr(fcs_file, fcs_keyword_map_single[key], nexus_file)
                     fcs_file.file_content_as_list.extend(cls.line_as_nexus_list(line, value, nexus_file))
                     fcs_file.includes_objects.append(nexus_file)
