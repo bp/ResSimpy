90f8e38eb9bf6dee1348c79aea205107efbfd12e
Jonny Hall <jonathan.hall@uk.bp.com>
2023-03-08 13:57:27 +0000

    move tests to below nexus simulator folder

--- tests/Nexus/nexus_simulator/conftest.py
+++ tests/Nexus/nexus_simulator/conftest.py
@@ -7,4 +7,5 @@ def simulation(mocker):
 
     listdir_mock = mocker.Mock(return_value=[])
     mocker.patch("os.listdir", listdir_mock)
+    # Create an override for isfile checks
     mocker.patch("os.path.isfile", lambda x: True)
