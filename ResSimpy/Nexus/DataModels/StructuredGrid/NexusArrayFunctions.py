d4d5d24bdb604949e36be344c278e64efad1ab91
isildirisil <isil.dirik@bp.com>
2023-03-21 19:48:12 +0000

    extended the tests to two array functions

--- ResSimpy/Nexus/DataModels/StructuredGrid/NexusArrayFunctions.py
+++ ResSimpy/Nexus/DataModels/StructuredGrid/NexusArrayFunctions.py
@@ -5,13 +5,15 @@ import ResSimpy.Nexus.array_function_operations as afo
 @dataclass
 class NexusArrayFunctions:
     """
-           Class to hold the array functions created with FUNCTION keyword in Nexus Structured Grid file
-
-           Attributes:
-               functions_summary_df: pd.DataFrame
+           Class to call the array functions created with FUNCTION keyword in Nexus Structured Grid file
 
            """
 
+# Not sure if we really need a class to call these functions, but here you go...
     @staticmethod
-    def load_functions_summary(file_content_as_list: list[str]):
+    def load_functions_list(file_content_as_list: list[str]):
+        return afo.collect_all_function_blocks(file_content_as_list)
+
+    @staticmethod
+    def load_functions_df(file_content_as_list: list[str]):
         return afo.summarize_model_functions(file_content_as_list)
