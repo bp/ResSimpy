64a65f184718a204cfaad5ec1d8947945b025150
Jonny Hall <jonathan.hall@uk.bp.com>
2023-03-29 17:36:12 +0100

    adds parent class abstract function

--- ResSimpy/NodeConnection.py
+++ ResSimpy/NodeConnection.py
@@ -8,10 +8,11 @@ from ResSimpy.Node import Node
 @dataclass
 class NodeConnection(ABC):
     name: Optional[str] = None
-    node_in: Optional[Node] = None
-    node_out: Optional[Node] = None
+    date: Optional[str] = None
+    node_in: Optional[str] = None
+    node_out: Optional[str] = None
     con_type: Optional[str] = None
     depth: Optional[float] = None
 
     def to_dict(self):
-        pass
+        raise NotImplementedError("Implement this in the derived class")
