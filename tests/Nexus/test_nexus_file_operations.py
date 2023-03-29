b09e561ae2fec645c8af88e8ee043399a21b32f0
Jonny Hall <jonathan.hall@uk.bp.com>
2023-03-29 17:36:49 +0100

    adds tests for collecting all tables in a nexusfile

--- tests/Nexus/test_nexus_file_operations.py
+++ tests/Nexus/test_nexus_file_operations.py
@@ -3,6 +3,8 @@ import ResSimpy.Nexus.nexus_file_operations as nfo
 import pytest
 import pandas as pd
 import numpy as np
+from ResSimpy.Nexus.DataModels.NexusFile import NexusFile
+from ResSimpy.Nexus.DataModels.Surface.NexusNode import NexusNode
 from ResSimpy.Nexus.NexusEnums.UnitsEnum import UnitSystem
 
 
@@ -483,6 +485,7 @@ def test_table_line_reader(headers, line, expected_dictionary, expected_valid_li
     ('NA', str, False, 'NA'),
     (None, str, False, None),
     (10.0, str, False, '10.0'),
+    ('None', str, False, None),
 ])
 def test_correct_datatypes(value, dtype, na_to_none, expected_result):
     # Act
@@ -572,3 +575,102 @@ def test_load_table_to_objects_date_units():
     # Assert
     assert result == expected_result
     assert result_unit_date == expected_result_unit_date
+
+
+@pytest.mark.parametrize('file_contents, node1_props, node2_props',[
+('''NODES
+  NAME                           TYPE       DEPTH   TEMP
+ ! Riser Nodes
+  node1                         NA            NA      #
+  node_2        WELLHEAD     1167.3 # 
+  ENDNODES
+''',
+{'name': 'node1', 'type': None, 'depth': None,  'temp': None, 'date': '01/01/2023', 'unit_system': UnitSystem.ENGLISH},
+{'name': 'node_2', 'type': 'WELLHEAD', 'depth': 1167.3, 'temp': None, 'date': '01/01/2023',
+    'unit_system': UnitSystem.ENGLISH}
+    ),
+('''NODES
+  NAME       TYPE       DEPTH   TemP    X     Y       NUMBER  StatiON
+ ! Riser Nodes
+  node1         NA        NA    60.5    100.5 300.5   1     station
+  node_2        WELLHEAD     1167.3 #  10.21085 3524.23 2   station2 ! COMMENT 
+  ENDNODES
+  content outside of the node statement
+  node1         NA        NA    60.5    10.5 3.5   1     station_null
+  ''',
+{'name': 'node1', 'type': None, 'depth': None, 'temp': 60.5, 'x_pos': 100.5, 'y_pos': 300.5, 'number': 1,
+    'station': 'station', 'date': '01/01/2023', 'unit_system': UnitSystem.ENGLISH},
+{'name': 'node_2', 'type': 'WELLHEAD', 'depth': 1167.3, 'temp': None, 'x_pos': 10.21085, 'y_pos': 3524.23, 'number': 2,
+    'station': 'station2', 'date': '01/01/2023', 'unit_system': UnitSystem.ENGLISH}
+  ),
+('''NODES
+  NAME                           TYPE       DEPTH   TEMP
+ ! Riser Nodes
+  node1                         NA            NA      #
+  ENDNODES
+  TIME 01/02/2023
+  NODES
+    NAME                           TYPE       DEPTH   TEMP
+
+  node_2        WELLHEAD     1167.3 # 
+  ENDNODES
+''',
+{'name': 'node1', 'type': None, 'depth': None,  'temp': None, 'date': '01/01/2023', 'unit_system': UnitSystem.ENGLISH},
+{'name': 'node_2', 'type': 'WELLHEAD', 'depth': 1167.3, 'temp': None, 'date': '01/02/2023',
+    'unit_system': UnitSystem.ENGLISH}
+    ),
+('''NODES
+  NAME                           TYPE       DEPTH   TEMP
+ ! Riser Nodes
+  node1                         NA            NA      #
+  ENDNODES
+  METRIC
+  TIME 01/02/2023
+  NODES
+    NAME                           TYPE       DEPTH   TEMP
+
+  node_2        WELLHEAD     1167.3 # 
+  ENDNODES
+''',
+{'name': 'node1', 'type': None, 'depth': None,  'temp': None, 'date': '01/01/2023', 'unit_system': UnitSystem.ENGLISH},
+{'name': 'node_2', 'type': 'WELLHEAD', 'depth': 1167.3, 'temp': None, 'date': '01/02/2023',
+    'unit_system': UnitSystem.METRIC}
+    ),
+('''NODES
+  NAME       TYPE       DEPTH   TemP    X     Y       NUMBER  StatiON
+ ! Riser Nodes
+  node1         NA        NA    60.5    100.5 300.5   1     station
+  ENDNODES
+  WELLS
+    NAME       TYPE       DEPTH   TemP       NUMBER  StatiON    
+node_2        WELLHEAD     1167.3 #  2   station2 
+ENDWELLS
+  content outside of the node statement
+  node1         NA        NA    60.5    10.5 3.5   1     station_null
+  ''',
+{'name': 'node1', 'type': None, 'depth': None, 'temp': 60.5, 'x_pos': 100.5, 'y_pos': 300.5, 'number': 1,
+    'station': 'station', 'date': '01/01/2023', 'unit_system': UnitSystem.ENGLISH},
+{'name': 'node_2', 'type': 'WELLHEAD', 'depth': 1167.3, 'temp': None, 'x_pos': None, 'y_pos': None, 'number': 2,
+    'station': 'station2', 'date': '01/01/2023', 'unit_system': UnitSystem.ENGLISH}
+  ),
+],
+ids=['basic', 'all columns', 'times', 'units', 'two tables']
+)
+def test_collect_all_tables_to_objects(mocker, file_contents, node1_props, node2_props):
+    # Arrange
+    # mock out a surface file:
+    start_date = '01/01/2023'
+    surface_file = NexusFile(location='surface.dat', file_content_as_list=file_contents.splitlines())
+
+    node_1 = NexusNode(node1_props)
+    node_2 = NexusNode(node2_props)
+
+    expected_result = [node_1, node_2]
+
+    # Act
+
+    result = nfo.collect_all_tables_to_objects(surface_file, NexusNode, ['NODES', 'WELLS'], start_date,
+                                               default_units=UnitSystem.ENGLISH)
+
+    # Assert
+    assert result == expected_result
