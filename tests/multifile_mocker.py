c045e95c4d9d8601e1e51bd00703f97db1a3337d
Jonny Hall <jonathan.hall@uk.bp.com>
2023-03-30 14:14:07 +0100

    fixes typo

--- tests/multifile_mocker.py
+++ tests/multifile_mocker.py
@@ -4,7 +4,7 @@ from pytest_mock import MockerFixture
 def mock_multiple_files(mocker: MockerFixture, passed_filename: str, potential_file_dict: dict[str, str]):
     """Mock method that returns different test file contents that are stored in the potential file dict
     Called via a mocker wrapper within a test for example:
-        from multiplefile_mocker import mock_multiple_files
+        from multifile_mocker import mock_multiple_files
         def mock_open_wrapper(filename, mode):
             mock_open = mock_multiple_files(mocker, filename, potential_file_dict={'key':value, }).return_value
             return mock_open
