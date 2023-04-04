c977cd48ef720119d1cacb4d6fafa0202fa947a2
Jonny Hall <jonathan.hall@uk.bp.com>
2023-04-04 15:46:58 +0100

    adds docstrings to new methods

--- ResSimpy/Nexus/NexusNetwork.py
+++ ResSimpy/Nexus/NexusNetwork.py
@@ -7,6 +7,7 @@ from ResSimpy.Nexus.DataModels.Network.NexusNodeConnection import NexusNodeConne
 from ResSimpy.Nexus.DataModels.Network.NexusNodeConnections import NexusNodeConnections
 from ResSimpy.Nexus.DataModels.Network.NexusNodes import NexusNodes
 import ResSimpy.Nexus.nexus_file_operations as nfo
+from ResSimpy.Nexus.DataModels.NexusFile import NexusFile
 
 if TYPE_CHECKING:
     from ResSimpy.Nexus.NexusSimulator import NexusSimulator
@@ -18,7 +19,17 @@ class NexusNetwork:
     Nodes: NexusNodes = NexusNodes()
     Connections: NexusNodeConnections = NexusNodeConnections()
 
-    def get_surface_file(self, method_number: Optional[int] = None):
+    def get_surface_file(self, method_number: Optional[int] = None) -> Optional[dict[int, NexusFile]]:
+        """ gets a specific surface file object or a dictionary of surface files keyed by method number
+
+        Args:
+            method_number (int): Method number for selection of a specific surface file.
+                If None then returns a dictionary of method, surface file object
+
+        Returns:
+            Optional[dict[int, NexusFile]]: returns a specific surface file object or a dictionary of surface files \
+                keyed by method number
+        """
         if method_number is None:
             return self.model.fcs_file.surface_files
         if self.model.fcs_file.surface_files is None:
@@ -26,6 +37,8 @@ class NexusNetwork:
         return self.model.fcs_file.surface_files[method_number]
 
     def load(self):
+        """ Loads all the objects from the surface files in the Simulator class.
+        """
         for surface in self.model.fcs_file.surface_files.values():
             nexus_obj_dict = nfo.collect_all_tables_to_objects(
                 surface, {'NODECON': NexusNodeConnection,
