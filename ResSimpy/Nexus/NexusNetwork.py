b03a88e15084e6084b78ca9f7f8a01211f22daec
Jonny Hall <jonathan.hall@uk.bp.com>
2023-04-05 10:32:09 +0100

    adds the well connections to the Network module

--- ResSimpy/Nexus/NexusNetwork.py
+++ ResSimpy/Nexus/NexusNetwork.py
@@ -7,6 +7,8 @@ from ResSimpy.Nexus.DataModels.Network.NexusNodeConnection import NexusNodeConne
 from ResSimpy.Nexus.DataModels.Network.NexusNodeConnections import NexusNodeConnections
 from ResSimpy.Nexus.DataModels.Network.NexusNodes import NexusNodes
 import ResSimpy.Nexus.nexus_file_operations as nfo
+from ResSimpy.Nexus.DataModels.Network.NexusWellConnection import NexusWellConnection
+from ResSimpy.Nexus.DataModels.Network.NexusWellConnections import NexusWellConnections
 from ResSimpy.Nexus.DataModels.NexusFile import NexusFile
 
 if TYPE_CHECKING:
@@ -18,6 +20,7 @@ class NexusNetwork:
     model: NexusSimulator
     Nodes: NexusNodes = NexusNodes()
     Connections: NexusNodeConnections = NexusNodeConnections()
+    WellConnections: NexusWellConnections = NexusWellConnections()
 
     def get_surface_file(self, method_number: Optional[int] = None) -> Optional[dict[int, NexusFile] | NexusFile]:
         """ gets a specific surface file object or a dictionary of surface files keyed by method number
@@ -43,8 +46,10 @@ class NexusNetwork:
             nexus_obj_dict = nfo.collect_all_tables_to_objects(
                 surface, {'NODECON': NexusNodeConnection,
                           'NODES': NexusNode,
+                          'WELLS': NexusWellConnection,
                           },
                 start_date=self.model.start_date,
                 default_units=self.model.get_default_units())
             self.Nodes.add_nodes(nexus_obj_dict.get('NODES'))
             self.Connections.add_connections(nexus_obj_dict.get('NODECON'))
+            self.WellConnections.add_connections(nexus_obj_dict.get('WELLS'))
