ea2841c37905350162b7b2a0cdb93778ac6c0941
Jonny Hall <jonathan.hall@uk.bp.com>
2023-04-06 17:53:27 +0100

    adds to dict method and nexus mapping

--- ResSimpy/Nexus/DataModels/Network/NexusConstraint.py
+++ ResSimpy/Nexus/DataModels/Network/NexusConstraint.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 from typing import Optional
 
 from ResSimpy.Constraint import Constraint
+from ResSimpy.Utils import to_dict_generic
 
 
 @dataclass
@@ -19,3 +20,22 @@ class NexusConstraint(Constraint):
         super().__init__()
         for key, prop in properties_dict.items():
             self.__setattr__(key, prop)
+
+    @staticmethod
+    def get_nexus_mapping() -> dict[str, tuple[str, type]]:
+        """gets the mapping of nexus keywords to attribute definitions"""
+        nexus_mapping = {}
+        return nexus_mapping
+
+    def to_dict(self, keys_in_nexus_style: bool = False) -> dict[str, None | str | int | float]:
+        """
+            Returns a dictionary of the attributes of the Constraint
+        Args:
+            keys_in_nexus_style (bool): if True returns the key values in Nexus keywords, otherwise returns the \
+                attribute name as stored by ressimpy
+
+        Returns:
+            a dictionary keyed by attributes and values as the value of the attribute
+        """
+        result_dict = to_dict_generic.to_dict(self, keys_in_nexus_style, add_date=True, add_units=True)
+        return result_dict
