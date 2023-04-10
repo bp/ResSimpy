0509657604520046e90de1528d7a41604c03d5f4
Jonny Hall <jonathan.hall@uk.bp.com>
2023-04-10 10:20:30 +0100

    remove unnecessary import

--- ResSimpy/Nexus/DataModels/Network/NexusNodeConnections.py
+++ ResSimpy/Nexus/DataModels/Network/NexusNodeConnections.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 from dataclasses import dataclass, field
-import numpy as np
 import pandas as pd
 from typing import Sequence, Optional, TYPE_CHECKING
 import ResSimpy.Nexus.nexus_file_operations as nfo
