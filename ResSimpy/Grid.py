58fb767e122954f7d7e873dca3e5a3669d9c2d48
Chris Flynn <chris@flynnsoftware.com>
2023-04-13 10:55:03 +0100

    Loading in the Structured Grid file properties only when they are needed (#74)
    * Loading in the Structured Grid file properties only when they are needed.
    
    * Fixing mypy issues.
    
    * Addressing review comments.

--- ResSimpy/Grid.py
+++ ResSimpy/Grid.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from abc import ABC
+from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from typing import Optional
 
@@ -13,28 +13,82 @@ class VariableEntry:
 @dataclass(kw_only=True)
 class Grid(ABC):
     # Grid dimensions
-    range_x: Optional[int]
-    range_y: Optional[int]
-    range_z: Optional[int]
+    _range_x: Optional[int]
+    _range_y: Optional[int]
+    _range_z: Optional[int]
 
-    netgrs: VariableEntry = VariableEntry()
-    porosity: VariableEntry = VariableEntry()
-    sw: VariableEntry = VariableEntry()
-    kx: VariableEntry = VariableEntry()
-    ky: VariableEntry = VariableEntry()
-    kz: VariableEntry = VariableEntry()
+    _netgrs: VariableEntry = VariableEntry()
+    _porosity: VariableEntry = VariableEntry()
+    _sw: VariableEntry = VariableEntry()
+    _kx: VariableEntry = VariableEntry()
+    _ky: VariableEntry = VariableEntry()
+    _kz: VariableEntry = VariableEntry()
 
     def __init__(self) -> None:
-        self.netgrs = VariableEntry()
-        self.porosity = VariableEntry()
-        self.sw = VariableEntry()
-        self.kx = VariableEntry()
-        self.ky = VariableEntry()
-        self.kz = VariableEntry()
+        self._netgrs = VariableEntry()
+        self._porosity = VariableEntry()
+        self._sw = VariableEntry()
+        self._kx = VariableEntry()
+        self._ky = VariableEntry()
+        self._kz = VariableEntry()
         # Grid dimensions
-        self.range_x: Optional[int] = None
-        self.range_y: Optional[int] = None
-        self.range_z: Optional[int] = None
+        self._range_x: Optional[int] = None
+        self._range_y: Optional[int] = None
+        self._range_z: Optional[int] = None
 
-    def load_structured_grid_file(self, structure_grid_file) -> Grid:
+    @property
+    def range_x(self):
+        self.load_grid_properties_if_not_loaded()
+        return self._range_x
+
+    @property
+    def range_y(self):
+        self.load_grid_properties_if_not_loaded()
+        return self._range_y
+
+    @property
+    def range_z(self):
+        self.load_grid_properties_if_not_loaded()
+        return self._range_z
+
+    @property
+    def netgrs(self):
+        self.load_grid_properties_if_not_loaded()
+        return self._netgrs
+
+    @property
+    def porosity(self):
+        self.load_grid_properties_if_not_loaded()
+        return self._porosity
+
+    @property
+    def sw(self):
+        self.load_grid_properties_if_not_loaded()
+        return self._sw
+
+    @property
+    def kx(self):
+        self.load_grid_properties_if_not_loaded()
+        return self._kx
+
+    @property
+    def ky(self):
+        self.load_grid_properties_if_not_loaded()
+        return self._ky
+
+    @property
+    def kz(self):
+        self.load_grid_properties_if_not_loaded()
+        return self._kz
+
+    @abstractmethod
+    def load_structured_grid_file(self, structure_grid_file, lazy_loading) -> Grid:
+        raise NotImplementedError("Implement this in the derived class")
+
+    @abstractmethod
+    def load_grid_properties_if_not_loaded(self) -> None:
+        raise NotImplementedError("Implement this in the derived class")
+
+    @abstractmethod
+    def to_dict(self) -> dict[str, Optional[int] | VariableEntry]:
         raise NotImplementedError("Implement this in the derived class")
