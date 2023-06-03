e3f38a2d304266164c5a0dd62080c92744111c00
Obiajulu Isebor <obiajulu.isebor@bp.com>
2023-06-03 16:06:33 -0500

    created Nexus hydraulics methods reader

---
+++ ResSimpy/Nexus/DataModels/NexusHydraulicsMethod.py
@@ -0,0 +1,166 @@
+from dataclasses import dataclass, field
+from enum import Enum
+import re
+from typing import Optional, Union
+import pandas as pd
+from ResSimpy.Nexus.DataModels.NexusFile import NexusFile
+from ResSimpy.Nexus.NexusKeywords.hyd_keywords import HYD_ARRAY_KEYWORDS, HYD_TABLE_HEADER_COLS, HYD_KEYWORDS
+from ResSimpy.Nexus.NexusKeywords.hyd_keywords import HYD_PRESSURE_KEYWORDS, HYD_SINGLE_KEYWORDS
+from ResSimpy.Nexus.NexusKeywords.hyd_keywords import HYD_KEYWORDS_VALUE_FLOAT, HYD_WATINJ_KEYWORDS_VALUE_FLOAT
+from ResSimpy.Nexus.NexusKeywords.hyd_keywords import HYD_ALQ_KEYWORD, HYD_ALQ_OPTIONS
+from ResSimpy.HydraulicsMethod import HydraulicsMethod
+
+from ResSimpy.Utils.factory_methods import get_empty_dict_union
+import ResSimpy.Nexus.nexus_file_operations as nfo
+
+
+@dataclass(kw_only=True)  # Doesn't need to write an _init_, _eq_ methods, etc.
+class NexusHydraulicsMethod(HydraulicsMethod):
+    """Class to hold Nexus Hydraulics properties
+    Attributes:
+        file_path (str): Path to the Nexus hydraulics properties file
+        method_number (int): Hydraulics properties method number in Nexus fcs file
+        properties (dict[str, Union[str, int, float, Enum, list[str], pd.DataFrame, dict[str, pd.DataFrame]]] ):
+            Dictionary holding all properties for a specific hydraulics properties method. Defaults to empty dictionary.
+    """
+
+    # General parameters
+    file_path: str
+    properties: dict[str, Union[str, int, float, Enum, list[str], pd.DataFrame,
+                     dict[str, Union[float, pd.DataFrame]]]] = field(default_factory=get_empty_dict_union)
+
+    def __init__(self, file_path: str, method_number: int,
+                 properties: Optional[dict[str, Union[str, int, float, Enum, list[str], pd.DataFrame,
+                                      dict[str, Union[float, pd.DataFrame]]]]] = None) -> None:
+        self.file_path = file_path
+        if properties is not None:
+            self.properties = properties
+        else:
+            self.properties = {}
+        super().__init__(method_number=method_number)
+
+    def read_properties(self) -> None:
+        """Read Nexus hydraulics file contents and populate the NexusHydraulicsMethod object."""
+        file_obj = NexusFile.generate_file_include_structure(self.file_path, origin=None)
+        file_as_list0 = file_obj.get_flat_list_str_file
+
+        # Loop through file_as_list and fix any line continuations that use '>' at the end of the line
+        file_as_list: list[str] = []
+        if len(file_as_list0) > 0:
+            file_as_list = [file_as_list0[0]]
+            line_indx = 1
+            while line_indx < len(file_as_list0):
+                prev_line = file_as_list[-1]
+                line = file_as_list0[line_indx]
+                if prev_line.endswith(('>', '>\n')):
+                    file_as_list[-1] = prev_line.split('>')[0] + ' ' + line.strip()
+                else:
+                    file_as_list.append(line)
+                line_indx += 1
+
+        # Check for common input data
+        nfo.check_for_and_populate_common_input_data(file_as_list, self.properties)
+
+        # Initialize flags and containers to use to record properties as we iterate through aquifer file contents
+        # Dictionary to record start and ending indices for tables
+        hyd_table_indices: dict[str, list[int]] = {}
+        # Dictionary of flags indicating which tables are being read
+        table_being_read: dict[str, bool] = {'HYD_TABLE': False, 'LIMITS': False}
+        start_reading_table = False
+        # Containers for handling WATERINJ data
+        watinj_dict: dict[str, float] = {}
+        found_waterinj = False
+
+        line_indx = 0
+        for line in file_as_list:
+
+            # Find arrays of parameters, e.g., QOIL 1.0 10. 100., or GOR 0.0 0.5 1.0
+            if [i for i in line.split() if i in HYD_ARRAY_KEYWORDS + HYD_ALQ_KEYWORD]:
+                for word in HYD_ARRAY_KEYWORDS + HYD_ALQ_KEYWORD:
+                    if nfo.check_token(word, line) and not table_being_read['LIMITS']:
+                        line_elems = line.split('!')[0].split()
+                        next_val = nfo.get_expected_token_value(word, line, file_as_list)
+                        if word == 'ALQ' and next_val in HYD_ALQ_OPTIONS:
+                            self.properties['ALQ_PARAM'] = next_val
+                            keyword_index = line_elems.index(next_val)
+                        else:
+                            keyword_index = line_elems.index(word)
+                        self.properties[word] = ' '.join(line_elems[keyword_index+1:])
+
+            # Create WATINJ property if needed
+            if nfo.check_token('WATINJ', line):
+                found_waterinj = True
+
+            # Handle DATGRAD property
+            if nfo.check_token('DATGRAD', line):
+                self.properties['DATGRAD'] = nfo.get_expected_token_value('DATGRAD', line, file_as_list)
+
+            # Find HYD key-value pairs, such as LENGTH 3000, DATUM 7000 or DATGRAD GRAD
+            if [i for i in line.split() if i in HYD_KEYWORDS_VALUE_FLOAT + HYD_WATINJ_KEYWORDS_VALUE_FLOAT]:
+                for key in HYD_KEYWORDS_VALUE_FLOAT + HYD_WATINJ_KEYWORDS_VALUE_FLOAT:
+                    if nfo.check_token(key, line):
+                        if found_waterinj and key in HYD_WATINJ_KEYWORDS_VALUE_FLOAT:
+                            watinj_dict[key] = float(nfo.get_expected_token_value(key, line, file_as_list))
+                        elif key in HYD_KEYWORDS_VALUE_FLOAT:
+                            self.properties[key] = float(nfo.get_expected_token_value(key, line, file_as_list))
+
+            # Find standalone hydraulics keywords
+            if [i for i in line.split() if i in HYD_SINGLE_KEYWORDS]:
+                for word in HYD_SINGLE_KEYWORDS:
+                    if nfo.check_token(word, line):
+                        self.properties[word] = ''
+
+            # Find starting and ending indices for hydraulic limits table
+            if nfo.check_token('LIMITS', line):
+                hyd_table_indices['LIMITS'] = [line_indx+1, len(file_as_list)]
+                table_being_read['LIMITS'] = True
+                start_reading_table = True
+            if nfo.check_token('ENDLIMITS', line):
+                hyd_table_indices['LIMITS'][1] = line_indx
+                table_being_read['LIMITS'] = False
+                start_reading_table = False
+
+            # Find ending index of hydraulics table
+            if start_reading_table and table_being_read['HYD_TABLE']:
+                for potential_endkeyword in HYD_KEYWORDS:
+                    if nfo.check_token(potential_endkeyword, line):
+                        hyd_table_indices['HYD_TABLE'][1] = line_indx
+                        start_reading_table = False
+                        table_being_read['HYD_TABLE'] = False
+                        break
+            # Find starting index of hydraulics table
+            for header_keyword in HYD_TABLE_HEADER_COLS:
+                if nfo.check_token(header_keyword, line):
+                    hyd_table_indices['HYD_TABLE'] = [line_indx, len(file_as_list)]
+                    table_being_read['HYD_TABLE'] = True
+                    start_reading_table = True
+
+            line_indx += 1
+
+        # Read in hydraulics tables
+        for key in hyd_table_indices.keys():
+            if key == 'HYD_TABLE':
+                # Get hydraulic table data
+                hyd_df: pd.DataFrame = nfo.read_table_to_df(file_as_list[
+                    hyd_table_indices[key][0]+1:hyd_table_indices[key][1]], noheader=True)
+                # Resolve hydraulic table header
+                hyd_df_header_names = [header_name for header_name in file_as_list[
+                    hyd_table_indices[key][0]].split('!')[0].split() if not re.search('BHP', header_name)]
+                pressure_keyword_map = {'PIN': 'POUT', 'POUT': 'PIN', 'THP': 'BHP'}
+                hyd_table_pressure_key = ''  # Determines what type of pressures are in table, i.e., PIN, POUT or BHP
+                for pkey in HYD_PRESSURE_KEYWORDS:
+                    if pkey in self.properties.keys():
+                        hyd_table_pressure_key = pressure_keyword_map[pkey]
+                        break
+                hyd_df_header_names = hyd_df_header_names + \
+                    [hyd_table_pressure_key+str(i) for i in range(hyd_df.shape[1] - len(hyd_df_header_names))]
+                hyd_df.columns = hyd_df_header_names
+                # Set appropriate hydraulic table property
+                self.properties[key] = hyd_df
+            else:
+                self.properties[key] = nfo.read_table_to_df(file_as_list[
+                    hyd_table_indices[key][0]:hyd_table_indices[key][1]])
+
+        # Correctly set WATINJ properties, if needed
+        if found_waterinj:
+            self.properties['WATINJ'] = watinj_dict
---
+++ tests/Nexus/test_nexus_hydraulics_class.py
@@ -0,0 +1,238 @@
+import pandas as pd
+import pytest
+
+from ResSimpy.Nexus.DataModels.NexusHydraulicsMethod import NexusHydraulicsMethod
+from ResSimpy.Nexus.NexusEnums.UnitsEnum import UnitSystem
+
+@pytest.mark.parametrize("file_contents, expected_hydraulics_properties",
+    [(
+    """
+    DESC Hydraulics Data
+
+    ENGLISH
+
+    ! Comment
+    QOIL 1.0 1000. 3000.
+    GOR 0.0 0.5     !  comment ! Gas oil ratio
+    ! Another comment
+    WCUT 0.0
+    THP 100. 500.
+    IGOR IWCUT IQOIL BHP(ITHP)
+       1     1     1  2470. 2545.
+       1     1     2  2478. 2548.
+       1     1     3  2493. 2569.
+       2     1     1  1860. 1990.
+       2     1     2  1881. 2002.
+       2     1     3  1947. 2039.
+    """, {'DESC': ['Hydraulics Data'],
+          'UNIT_SYSTEM': UnitSystem.ENGLISH,
+          'QOIL': '1.0 1000. 3000.',
+          'GOR': '0.0 0.5',
+          'WCUT': '0.0',
+          'THP': '100. 500.',
+          'HYD_TABLE': pd.DataFrame({'IGOR': [1, 1, 1, 2, 2, 2],
+                                     'IWCUT': [1, 1, 1, 1, 1, 1],
+                                     'IQOIL': [1, 2, 3, 1, 2, 3],
+                                     'BHP0': [2470., 2478., 2493., 1860., 1881., 1947.],
+                                     'BHP1': [2545., 2548., 2569., 1990., 2002., 2039.0]
+                                     })
+          }
+    ),
+    (
+    """
+    DESC Hydraulics Data
+
+    ENGLISH
+
+    ! Comment
+    QLIQ 1.0 1000. 3000.
+    GLR 0.0 0.5     !  comment ! Gas oil ratio
+    ! Another comment
+    WCUT 0.0
+    POUT 100. 500.
+
+    LENGTH 10000
+    DATUM 8000
+
+    IGLR IWCUT IQLIQ
+       1     1     1  2470. 2545.
+       1     1     2  2478. 2548.
+       1     1     3  2493. 2569.
+       2     1     1  1860. 1990.
+       2     1     2  1881. 2002.
+       2     1     3  1947. 2039.
+
+    NOCHK
+
+    WATINJ GRAD 0.433
+           VISC 0.7
+           LENGTH 9000
+           ROUGHNESS 1e-5
+           DZ 8000
+           DIAM 7
+
+    LIMITS
+    VARIABLE MIN MAX
+    QLIQ 0.0 5000.0
+    GLR 0.0 1.0
+    ENDLIMITS
+
+    DATGRAD GRAD
+    """, {'DESC': ['Hydraulics Data'],
+          'UNIT_SYSTEM': UnitSystem.ENGLISH,
+          'QLIQ': '1.0 1000. 3000.',
+          'GLR': '0.0 0.5',
+          'WCUT': '0.0',
+          'POUT': '100. 500.',
+          'LENGTH': 10000., 'DATUM': 8000., 'NOCHK': '', 'DATGRAD': 'GRAD',
+          'WATINJ': {'GRAD': 0.433, 'VISC': 0.7, 'LENGTH': 9000,
+                     'ROUGHNESS': 1e-5, 'DZ': 8000, 'DIAM': 7},
+          'HYD_TABLE': pd.DataFrame({'IGLR': [1, 1, 1, 2, 2, 2],
+                                     'IWCUT': [1, 1, 1, 1, 1, 1],
+                                     'IQLIQ': [1, 2, 3, 1, 2, 3],
+                                     'PIN0': [2470., 2478., 2493., 1860., 1881., 1947.],
+                                     'PIN1': [2545., 2548., 2569., 1990., 2002., 2039.0]
+                                     }),
+          'LIMITS': pd.DataFrame({'VARIABLE': ['QLIQ', 'GLR'],
+                                  'MIN': [0., 0.],
+                                  'MAX': [5000., 1.]
+                                  })
+          }
+    ),
+    (
+    """
+    DESC Hydraulics Data
+
+    ENGLISH
+
+    ! Comment
+    QOIL 1.0 1000. 3000.
+    GOR 0.0 0.5     !  comment ! Gas oil ratio
+    ! Another comment
+    WCUT 0.0
+    ALQ  0.0 50.0
+    THP 100. 500. 900. 1400. 2000.
+    IGOR IWCUT  IALQ  IQOIL  BHP(ITHP)
+       1     1     1      1  2470. 2545. 2600. 2820. 3070.
+       1     1     1      2  2478. 2548. 2613. 2824. 3081.
+       1     1     1      3  2493. 2569. 2638. 2870. 3138.
+       1     1     2      1  2535. 2600. 2650. 2852. 3130.
+       1     1     2      2  2541. 2608. 2673. 2884. 3141.
+       1     1     2      3  2555. 2631. 2703. 2931. 3197.
+       2     1     1      1  1860. 1990. 2090. 2435. 2830.
+       2     1     1      2  1881. 2002. 2101. 2438. 2836.
+       2     1     1      3  1947. 2039. 2131. 2448. 2848.
+       2     1     2      1  1990. 2100. 2190. 2530. 2916.
+       2     1     2      2  2004. 2109. 2206. 2537. 2926.
+       2     1     2      3  2033. 2130. 2224. 2548. 2946.
+    """, {'DESC': ['Hydraulics Data'],
+          'UNIT_SYSTEM': UnitSystem.ENGLISH,
+          'QOIL': '1.0 1000. 3000.',
+          'GOR': '0.0 0.5',
+          'WCUT': '0.0',
+          'ALQ': '0.0 50.0',
+          'THP': '100. 500. 900. 1400. 2000.',
+          'HYD_TABLE': pd.DataFrame({'IGOR': [1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2],
+                                     'IWCUT': [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                                     'IALQ': [1, 1, 1, 2, 2, 2, 1, 1, 1, 2, 2, 2],
+                                     'IQOIL': [1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3],
+                                     'BHP0': [2470., 2478., 2493., 2535., 2541., 2555.,
+                                              1860., 1881., 1947., 1990., 2004., 2033.],
+                                     'BHP1': [2545., 2548., 2569., 2600., 2608., 2631.,
+                                              1990., 2002., 2039., 2100., 2109., 2130.],
+                                     'BHP2': [2600., 2613., 2638., 2650., 2673., 2703.,
+                                              2090., 2101., 2131., 2190., 2206., 2224.],
+                                     'BHP3': [2820., 2824., 2870., 2852., 2884., 2931.,
+                                              2435., 2438., 2448., 2530., 2537., 2548.],
+                                     'BHP4': [3070., 3081., 3138., 3130., 3141., 3197.,
+                                              2830., 2836., 2848., 2916., 2926., 2946.]
+                                     })
+          }
+    ),
+    (
+    """
+    DESC Hydraulics Data
+
+    ENGLISH
+
+    ! Comment
+    QOIL 1.0 1000. 3000.
+    GOR 0.0 0.5     !  comment ! Gas oil ratio
+    ! Another comment
+    WCUT 0.0
+    ALQ     GASRATE     0.0     50.0
+    THP 100. 500. 900. 1400. 2000.
+    IGOR IWCUT  IALQ  IQOIL  BHP(ITHP)
+       1     1     1      1  2470. 2545. >
+                             2600. 2820. >
+                             3070.
+       1     1     1      2  2478. 2548. >
+                             2613. 2824. >
+                             3081.
+       1     1     1      3  2493. 2569. 2638. 2870. 3138.
+       1     1     2      1  2535. 2600. 2650. 2852. 3130.
+       1     1     2      2  2541. 2608. 2673. 2884. 3141.
+       1     1     2      3  2555. 2631. 2703. 2931. 3197.
+       2     1     1      1  1860. 1990. 2090. 2435. 2830.
+       2     1     1      2  1881. 2002. 2101. 2438. 2836.
+       2     1     1      3  1947. 2039. 2131. 2448. 2848.
+       2     1     2      1  1990. 2100. 2190. 2530. 2916.
+       2     1     2      2  2004. 2109. >
+                             2206. 2537. >
+                             2926.
+       2     1     2      3  2033. 2130. >
+                             2224. 2548. >
+                             2946.
+    """, {'DESC': ['Hydraulics Data'],
+          'UNIT_SYSTEM': UnitSystem.ENGLISH,
+          'QOIL': '1.0 1000. 3000.',
+          'GOR': '0.0 0.5',
+          'WCUT': '0.0',
+          'ALQ': '0.0 50.0',
+          'ALQ_PARAM': 'GASRATE',
+          'THP': '100. 500. 900. 1400. 2000.',
+          'HYD_TABLE': pd.DataFrame({'IGOR': [1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2],
+                                     'IWCUT': [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                                     'IALQ': [1, 1, 1, 2, 2, 2, 1, 1, 1, 2, 2, 2],
+                                     'IQOIL': [1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3],
+                                     'BHP0': [2470., 2478., 2493., 2535., 2541., 2555.,
+                                              1860., 1881., 1947., 1990., 2004., 2033.],
+                                     'BHP1': [2545., 2548., 2569., 2600., 2608., 2631.,
+                                              1990., 2002., 2039., 2100., 2109., 2130.],
+                                     'BHP2': [2600., 2613., 2638., 2650., 2673., 2703.,
+                                              2090., 2101., 2131., 2190., 2206., 2224.],
+                                     'BHP3': [2820., 2824., 2870., 2852., 2884., 2931.,
+                                              2435., 2438., 2448., 2530., 2537., 2548.],
+                                     'BHP4': [3070., 3081., 3138., 3130., 3141., 3197.,
+                                              2830., 2836., 2848., 2916., 2926., 2946.]
+                                     })
+          }
+    )
+    ], ids=['basic_hyd', 'complex_hyd', 'with_alq', 'alq_gr_and_line_continuation']
+)
+def test_read_hydraulics_properties_from_file(mocker, file_contents, expected_hydraulics_properties):
+    # Arrange
+    hydraulics_obj = NexusHydraulicsMethod(file_path='test/file/hydraulics.dat', method_number=1)
+
+    # mock out open to return our test file contents
+    open_mock = mocker.mock_open(read_data=file_contents)
+    mocker.patch("builtins.open", open_mock)
+
+    # Act
+    hydraulics_obj.read_properties()
+    props = hydraulics_obj.properties
+
+    # Assert
+    for key in expected_hydraulics_properties:
+        if isinstance(expected_hydraulics_properties[key], pd.DataFrame):
+            pd.testing.assert_frame_equal(expected_hydraulics_properties[key], props[key])
+        elif isinstance(expected_hydraulics_properties[key], dict):
+            for subkey in expected_hydraulics_properties[key].keys():
+                if isinstance(expected_hydraulics_properties[key][subkey], pd.DataFrame):
+                    pd.testing.assert_frame_equal(expected_hydraulics_properties[key][subkey], props[key][subkey])
+                else:
+                    assert props[key][subkey] == expected_hydraulics_properties[key][subkey]
+        else:
+            assert props[key] == expected_hydraulics_properties[key]
+
+
