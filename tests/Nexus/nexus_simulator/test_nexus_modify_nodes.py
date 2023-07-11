f4fd4efd5f71ab25021a369306230034d27def90
Jonny Hall <jonathan.hall@uk.bp.com>
2023-07-11 17:43:06 +0100

    adds basic test for adding nodes and starting code for adding

--- tests/Nexus/nexus_simulator/test_nexus_modify_nodes.py
+++ tests/Nexus/nexus_simulator/test_nexus_modify_nodes.py
@@ -236,3 +236,72 @@ def test_remove_node(mocker, file_contents, expected_file_contents, node_to_remo
                                      modifying_mock_open=writing_mock_open,
                                      mocker_fixture=mocker, write_file_name='/surface_file_01.dat',
                                      number_of_writes=expected_number_writes)
+
+
+@pytest.mark.parametrize('file_contents, expected_file_contents, node_to_add, expected_nodes, expected_number_writes', [
+# basic_test
+('''TIME 01/01/2023
+NODES
+  NAME                           TYPE       DEPTH   TEMP
+  node1                         NA            NA      #
+  ENDNODES
+''',
+'''TIME 01/01/2023
+NODES
+  NAME                           TYPE       DEPTH   TEMP
+  node1                         NA            NA      #
+node_2 WELLHEAD 1167.3 NA
+  ENDNODES
+''',
+{'name': 'node_2', 'type': 'WELLHEAD', 'depth': 1167.3, 'temp': None, 'date': '01/01/2023', 'unit_system': UnitSystem.ENGLISH},
+[{'name': 'node1', 'type': None, 'depth': None,  'temp': None, 'date': '01/01/2023', 'unit_system': UnitSystem.ENGLISH},
+{'name': 'node_2', 'type': 'WELLHEAD', 'depth': 1167.3, 'temp': None, 'date': '01/01/2023',
+    'unit_system': UnitSystem.ENGLISH}],
+1  # no. writes
+),
+
+
+], ids=['basic_test', ])
+def test_add_node(mocker, file_contents, expected_file_contents, node_to_add, expected_nodes,
+                     expected_number_writes):
+    # Arrange
+    fcs_file_contents = '''
+        RUN_UNITS ENGLISH
+        DATEFORMAT DD/MM/YYYY
+        RECURRENT_FILES
+        RUNCONTROL /nexus_data/runcontrol.dat
+        SURFACE Network 1  /surface_file_01.dat
+        '''
+    runcontrol_contents = '''START 01/01/2019'''
+
+    def mock_open_wrapper(filename, mode):
+        mock_open = mock_multiple_files(mocker, filename, potential_file_dict={
+            '/path/fcs_file.fcs': fcs_file_contents,
+            '/surface_file_01.dat': file_contents,
+            '/nexus_data/runcontrol.dat': runcontrol_contents}
+            ).return_value
+        return mock_open
+    mocker.patch("builtins.open", mock_open_wrapper)
+    nexus_sim = get_fake_nexus_simulator(mocker, fcs_file_path='/path/fcs_file.fcs', mock_open=False)
+    # make a mock for the write operation
+    writing_mock_open = mocker.mock_open()
+    mocker.patch("builtins.open", writing_mock_open)
+
+    expected_nodes = [NexusNode(node) for node in expected_nodes]
+    expected_nodes.sort(key=lambda x: x.name)
+
+    mocker.patch.object(uuid, 'uuid4', side_effect=['uuid1', 'uuid2', 'uuid3', 'uuid4', 'uuid5', 'uuid6'])
+    # Act
+    nexus_sim.network.nodes.add_node(node_to_add)
+    # compare sets as order doesn't matter
+    result_nodes = nexus_sim.network.nodes.get_nodes()
+    result_nodes.sort(key=lambda x: x.name)
+    # Assert
+    assert result_nodes == expected_nodes
+    assert nexus_sim.model_files.surface_files[1].file_content_as_list == expected_file_contents.splitlines(keepends=True)
+    check_file_read_write_is_correct(expected_file_contents=expected_file_contents,
+                                     modifying_mock_open=writing_mock_open,
+                                     mocker_fixture=mocker, write_file_name='/surface_file_01.dat',
+                                     number_of_writes=expected_number_writes)
+
+    # TODO check line locations too
