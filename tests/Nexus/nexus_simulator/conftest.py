9b8d0e15c2697dcb83452feed994dbf8c13ec1dc
Sasanka Panchamukhi <Sasanka.panchamukhi@bp.com>
2023-07-28 15:18:19 +0530

    removed the method in multiple locations

--- tests/Nexus/nexus_simulator/conftest.py
+++ tests/Nexus/nexus_simulator/conftest.py
@@ -1,4 +1,5 @@
 import pytest
+from tests.utility_for_tests import get_fake_stat_pathlib_time
 
 
 @pytest.fixture(scope="function", autouse=True)
@@ -11,21 +12,7 @@ def simulation(mocker):
     mocker.patch("os.path.isfile", lambda x: True)
 
 @pytest.fixture
-def globalFixture(mocker,request):
-
-    path_mock = mocker.MagicMock()
-    mocker.patch('pathlib.Path', path_mock)
-    path_mock.return_value.owner.return_value = None
-    path_mock.return_value.group.return_value = None
-
-    os_mock = mocker.MagicMock()
-    mocker.patch('os.stat',os_mock)
-    os_mock.return_value.st_mtime = None
-
-    def sideeffect(timestamp=None):
-        return None
-    ts_mock = mocker.MagicMock()
-    mocker.patch('datetime.datetime',ts_mock)
-    ts_mock.return_value.fromtimestamp.side_effect = sideeffect
+def globalFixture(mocker):
+    get_fake_stat_pathlib_time(mocker)
 
 
