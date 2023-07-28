8f10acc38b080872cef799ccaf22c5f8de931dcd
Sasanka Panchamukhi <Sasanka.panchamukhi@bp.com>
2023-07-28 14:56:07 +0530

    modified fixture

--- tests/Nexus/nexus_simulator/conftest.py
+++ tests/Nexus/nexus_simulator/conftest.py
@@ -13,29 +13,19 @@ def simulation(mocker):
 @pytest.fixture
 def globalFixture(mocker,request):
 
-    if 'disable_autouse' in request.keywords:
-        path_mock = mocker.MagicMock()
-        mocker.patch('pathlib.Path', path_mock)
-        path_mock.return_value.owner.return_value = "Mock-Owner"
-        path_mock.return_value.group.return_value = "Mock-Group"
+    path_mock = mocker.MagicMock()
+    mocker.patch('pathlib.Path', path_mock)
+    path_mock.return_value.owner.return_value = None
+    path_mock.return_value.group.return_value = None
 
-        os_mock = mocker.MagicMock()
-        mocker.patch('os.stat',os_mock)
-        os_mock.return_value.st_mtime = 1530346690 
-    else:
-        path_mock = mocker.MagicMock()
-        mocker.patch('pathlib.Path', path_mock)
-        path_mock.return_value.owner.return_value = None
-        path_mock.return_value.group.return_value = None
+    os_mock = mocker.MagicMock()
+    mocker.patch('os.stat',os_mock)
+    os_mock.return_value.st_mtime = None
 
-        os_mock = mocker.MagicMock()
-        mocker.patch('os.stat',os_mock)
-        os_mock.return_value.st_mtime = None
-        
-        def sideeffect(timestamp=None):
-            return None
-        ts_mock = mocker.MagicMock()
-        mocker.patch('datetime.datetime',ts_mock)
-        ts_mock.return_value.fromtimestamp.side_effect = sideeffect
+    def sideeffect(timestamp=None):
+        return None
+    ts_mock = mocker.MagicMock()
+    mocker.patch('datetime.datetime',ts_mock)
+    ts_mock.return_value.fromtimestamp.side_effect = sideeffect
 
 
