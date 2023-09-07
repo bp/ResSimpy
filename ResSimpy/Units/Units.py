562e0e666e694678107f1d3e8a2bd44c7707efd9
jonnyh404 <121860375+jonnyh404@users.noreply.github.com>
2023-09-07 14:46:57 +0100

    add test cases

--- ResSimpy/Units/Units.py
+++ ResSimpy/Units/Units.py
@@ -5,40 +5,40 @@ from dataclasses import dataclass
 from ResSimpy.Enums.UnitsEnum import UnitSystem
 
 """
-Variable	English Oilfield Units	Metric Units (kPa)	Metric Units (kg/cm²)	Metric Units (bars)	Lab Units
-Acoustic Impedance	ft/sec·(g/cm³)	m/sec·(kg/m³)	m/sec·(kg/m³)	m/sec·(kg/m³)	cm/sec·(g/cm³)
+Variable	English Oilfield Units	Metric Units (kPa)	Metric Units (kg/cm2)	Metric Units (bars)	Lab Units
+Acoustic Impedance	ft/sec·(g/cm3)	m/sec·(kg/m3)	m/sec·(kg/m3)	m/sec·(kg/m3)	cm/sec·(g/cm3)
 Acoustic Wave Velocity	ft/sec	m/sec	m/sec	m/sec	m/sec
 Angle	degrees	degrees	degrees	degrees	degrees
-Area	ft²	m²	m²	m²	cm²
-Bulk Modulus	psia	kPa	kg/cm²	bars	psia
-Compressibility	psi⁻¹	kPa⁻¹	(kg/cm²)⁻¹	(bars)⁻¹	psi⁻¹
-Critical Pressure	psia	kPa	(kg/cm²)	bars	psia
+Area	ft2	m2	m2	m2	cm2
+Bulk Modulus	psia	kPa	kg/cm2	bars	psia
+Compressibility	psi-1	kPa-1	(kg/cm2)-1	(bars)-1	psi-1
+Critical Pressure	psia	kPa	(kg/cm2)	bars	psia
 Critical Temperature	degrees F	degrees C	degrees C	degrees C	degrees C
-Density	lb/ft³	kg/m³	kg/m³	kg/m³	gm/cc
-Formation Volume Factor (Oil)	RB/STB	m³/STM³	m³/STM³	m³/STM³	cc/stcc
-Formation Volume Factor (Gas)	RB/MSCF	m³/STM³	m³/STM³	m³/STM³	cc/stcc
-Gas-Liquid Ratio (Gas-Oil Ratio)	MSCF/STB	SM³/STM³	SM³/STM³	SM³/STM³	stcc/stcc
-Gravity Gradient	psi/ft	kPa/m	kg/cm²/m	bars/m	psi/cm
+Density	lb/ft3	kg/m3	kg/m3	kg/m3	gm/cc
+Formation Volume Factor (Oil)	RB/STB	m3/STM3	m3/STM3	m3/STM3	cc/stcc
+Formation Volume Factor (Gas)	RB/MSCF	m3/STM3	m3/STM3	m3/STM3	cc/stcc
+Gas-Liquid Ratio (Gas-Oil Ratio)	MSCF/STB	SM3/STM3	SM3/STM3	SM3/STM3	stcc/stcc
+Gravity Gradient	psi/ft	kPa/m	kg/cm2/m	bars/m	psi/cm
 Length	ft	m	m	m	cm
 Moles	lb-moles	kg-moles	kg-moles	kg-moles	gmmoles
-Molar Density	lb-moles/ft³	kg-moles/m³	kg-moles/m³	kg-moles/m³	gmmoles/cm³
+Molar Density	lb-moles/ft3	kg-moles/m3	kg-moles/m3	kg-moles/m3	gmmoles/cm3
 Molar Rates	lb-moles/day	kg-moles/day	kg-moles/day	kg-moles/day	gmmoles/hour
 Permeability	md	md	md	md	md
-Pressure	psia	kPa	kg/cm²	bars	psia
-Reservoir Rates	RB/day	m³/day	m³/day	m³/day	cc/hour
-Reservoir Volumes	MRB	k m³	k m³	k m³	k cc
+Pressure	psia	kPa	kg/cm2	bars	psia
+Reservoir Rates	RB/day	m3/day	m3/day	m3/day	cc/hour
+Reservoir Volumes	MRB	k m3	k m3	k m3	k cc
 Saturation Fraction	fraction	fraction	fraction	fraction	fraction
-Solution Gas-Oil Ratio	MSCF/STB	SM³/STM³	SM³/STM³	SM³/STM³	stcc/stcc
-Surface Rates (Liquid)	STB/day	STM³/day	STM³/day	STM³/day	stcc/hour
-Surface Rates (Gas)	MSCF/day	SM³/day	SM³/day	SM³/day	stcc/hour
-Surface Volumes (Gas)	MMSCF	k STM³	k STM³	k STM³	k stcc
-Surface Volumes (Liquid)	MSTB	k STM³	k STM³	k STM³	k stcc
+Solution Gas-Oil Ratio	MSCF/STB	SM3/STM3	SM3/STM3	SM3/STM3	stcc/stcc
+Surface Rates (Liquid)	STB/day	STM3/day	STM3/day	STM3/day	stcc/hour
+Surface Rates (Gas)	MSCF/day	SM3/day	SM3/day	SM3/day	stcc/hour
+Surface Volumes (Gas)	MMSCF	k STM3	k STM3	k STM3	k stcc
+Surface Volumes (Liquid)	MSTB	k STM3	k STM3	k STM3	k stcc
 Temperature	degrees F	degrees C	degrees C	degrees C	degrees C
 Time	days	days	days	days	hours
 Tracer Concentrations	fraction	fraction	fraction	fraction	fraction
-Transmissibility	ft³·cp/day/psi	m³·cp/day/kPa	m³·cp/day/kg/cm²	m³·cp/day/bars	cc·cp/hour/psi
+Transmissibility	ft3·cp/day/psi	m3·cp/day/kPa	m3·cp/day/kg/cm2	m3·cp/day/bars	cc·cp/hour/psi
 Viscosity	cp	cp	cp	cp	cp
-Volume	ft³	m³	m³  m³	cc
+Volume	ft3	m3	m3  m3	cc
 """
 
 
@@ -83,12 +83,12 @@ class Area(UnitDimension):
 
 class AcousticImpedance(UnitDimension):
     """Units for acoustic impedance."""
-    english = 'ft/sec·(g/cm³)'
-    metric = 'm/sec·(kg/m³)'
-    lab = 'cm/sec·(g/cm³)'
-    metkgcm2 = 'm/sec·(kg/m³)'
-    metbar = 'm/sec·(kg/m³)'
-    metric_atm = 'm/sec·(kg/m³)'
+    english = 'ft/sec·(g/cm3)'
+    metric = 'm/sec·(kg/m3)'
+    lab = 'cm/sec·(g/cm3)'
+    metkgcm2 = 'm/sec·(kg/m3)'
+    metbar = 'm/sec·(kg/m3)'
+    metric_atm = 'm/sec·(kg/m3)'
 
 
 class AcousticWaveVelocity(UnitDimension):
@@ -114,7 +114,7 @@ class BulkModulus(UnitDimension):
     """Units for bulk modulus."""
     english = 'psia'
     metric = 'kPa'
-    metkgcm2 = 'kg/cm²'
+    metkgcm2 = 'kg/cm2'
     metbar = 'bars'
     lab = 'psia'
     metric_atm = 'atm'
@@ -122,19 +122,19 @@ class BulkModulus(UnitDimension):
 
 class Compressibility(UnitDimension):
     """Units for compressibility."""
-    english = 'psi⁻¹'
-    metric = 'kPa⁻¹'
-    metkgcm2 = '(kg/cm²)⁻¹'
-    metbar = '(bars)⁻¹'
-    lab = 'psi⁻¹'
-    metric_atm = '(atm)⁻¹'
+    english = 'psi-1'
+    metric = 'kPa-1'
+    metkgcm2 = '(kg/cm2)-1'
+    metbar = '(bars)-1'
+    lab = 'psi-1'
+    metric_atm = '(atm)-1'
 
 
 class CriticalPressure(UnitDimension):
     """Units for critical pressure."""
     english = 'psia'
     metric = 'kPa'
-    metkgcm2 = 'kg/cm²'
+    metkgcm2 = 'kg/cm2'
     metbar = 'bars'
     lab = 'psia'
     metric_atm = 'atm'
@@ -151,49 +151,49 @@ class CriticalTemperature(UnitDimension):
 
 class Density(UnitDimension):
     """Units for density."""
-    english = 'lb/ft³'
-    metric = 'kg/m³'
-    metkgcm2 = 'kg/m³'
-    metbar = 'kg/m³'
+    english = 'lb/ft3'
+    metric = 'kg/m3'
+    metkgcm2 = 'kg/m3'
+    metbar = 'kg/m3'
     lab = 'gm/cc'
-    metric_atm = 'kg/m³'
+    metric_atm = 'kg/m3'
 
 
 class FormationVolumeFactorOil(UnitDimension):
     """Units for formation volume factor oil."""
     english = 'RB/STB'
-    metric = 'm³/STM³'
-    metkgcm2 = 'm³/STM³'
-    metbar = 'm³/STM³'
+    metric = 'm3/STM3'
+    metkgcm2 = 'm3/STM3'
+    metbar = 'm3/STM3'
     lab = 'cc/stcc'
-    metric_atm = 'm³/STM³'
+    metric_atm = 'm3/STM3'
 
 
 class FormationVolumeFactorGas(UnitDimension):
     """Units for formation volume factor gas."""
     english = 'RB/MSCF'
-    metric = 'm³/STM³'
-    metkgcm2 = 'm³/STM³'
-    metbar = 'm³/STM³'
+    metric = 'm3/STM3'
+    metkgcm2 = 'm3/STM3'
+    metbar = 'm3/STM3'
     lab = 'cc/stcc'
-    metric_atm = 'm³/STM³'
+    metric_atm = 'm3/STM3'
 
 
 class GasLiquidRatio(UnitDimension):
     """Units for gas liquid ratio."""
     english = 'MSCF/STB'
-    metric = 'SM³/STM³'
-    metkgcm2 = 'SM³/STM³'
-    metbar = 'SM³/STM³'
+    metric = 'SM3/STM3'
+    metkgcm2 = 'SM3/STM3'
+    metbar = 'SM3/STM3'
     lab = 'stcc/stcc'
-    metric_atm = 'SM³/STM³'
+    metric_atm = 'SM3/STM3'
 
 
 class GravityGradient(UnitDimension):
     """Units for gravity gradient."""
     english = 'psi/ft'
     metric = 'kPa/m'
-    metkgcm2 = 'kg/cm²/m'
+    metkgcm2 = 'kg/cm2/m'
     metbar = 'bars/m'
     lab = 'psi/cm'
     metric_atm = 'atm/m'
@@ -221,12 +221,12 @@ class Moles(UnitDimension):
 
 class MolarDensity(UnitDimension):
     """Units for molar density."""
-    english = 'lb-moles/ft³'
-    metric = 'kg-moles/m³'
-    metkgcm2 = 'kg-moles/m³'
-    metbar = 'kg-moles/m³'
-    lab = 'gmmoles/cm³'
-    metric_atm = 'kg-moles/m³'
+    english = 'lb-moles/ft3'
+    metric = 'kg-moles/m3'
+    metkgcm2 = 'kg-moles/m3'
+    metbar = 'kg-moles/m3'
+    lab = 'gmmoles/cm3'
+    metric_atm = 'kg-moles/m3'
 
 
 class MolarRates(UnitDimension):
@@ -253,7 +253,7 @@ class Pressure(UnitDimension):
     """Units for pressure."""
     english = 'psia'
     metric = 'kPa'
-    metkgcm2 = 'kg/cm²'
+    metkgcm2 = 'kg/cm2'
     metbar = 'bars'
     lab = 'psia'
     metric_atm = 'atm'
@@ -262,21 +262,21 @@ class Pressure(UnitDimension):
 class ReservoirRates(UnitDimension):
     """Units for reservoir rates."""
     english = 'RB/day'
-    metric = 'm³/day'
-    metkgcm2 = 'm³/day'
-    metbar = 'm³/day'
+    metric = 'm3/day'
+    metkgcm2 = 'm3/day'
+    metbar = 'm3/day'
     lab = 'cc/hour'
-    metric_atm = 'm³/day'
+    metric_atm = 'm3/day'
 
 
 class ReservoirVolumes(UnitDimension):
     """Units for reservoir volumes."""
     english = 'MRB'
-    metric = 'k m³'
-    metkgcm2 = 'k m³'
-    metbar = 'k m³'
+    metric = 'k m3'
+    metkgcm2 = 'k m3'
+    metbar = 'k m3'
     lab = 'k cc'
-    metric_atm = 'k m³'
+    metric_atm = 'k m3'
 
 
 class SaturationFraction(UnitDimension):
@@ -292,51 +292,51 @@ class SaturationFraction(UnitDimension):
 class SolutionGasOilRatio(UnitDimension):
     """Units for solution gas oil ratio."""
     english = 'MSCF/STB'
-    metric = 'SM³/STM³'
-    metkgcm2 = 'SM³/STM³'
-    metbar = 'SM³/STM³'
+    metric = 'SM3/STM3'
+    metkgcm2 = 'SM3/STM3'
+    metbar = 'SM3/STM3'
     lab = 'stcc/stcc'
-    metric_atm = 'SM³/STM³'
+    metric_atm = 'SM3/STM3'
 
 
 class SurfaceRatesLiquid(UnitDimension):
     """Units for surface rates liquid."""
     english = 'STB/day'
-    metric = 'STM³/day'
-    metkgcm2 = 'STM³/day'
-    metbar = 'STM³/day'
+    metric = 'STM3/day'
+    metkgcm2 = 'STM3/day'
+    metbar = 'STM3/day'
     lab = 'stcc/hour'
-    metric_atm = 'STM³/day'
+    metric_atm = 'STM3/day'
 
 
 class SurfaceRatesGas(UnitDimension):
     """Units for surface rates gas."""
     english = 'MSCF/day'
-    metric = 'SM³/day'
-    metkgcm2 = 'SM³/day'
-    metbar = 'SM³/day'
+    metric = 'SM3/day'
+    metkgcm2 = 'SM3/day'
+    metbar = 'SM3/day'
     lab = 'stcc/hour'
-    metric_atm = 'SM³/day'
+    metric_atm = 'SM3/day'
 
 
 class SurfaceVolumesGas(UnitDimension):
     """Units for surface volumes gas."""
     english = 'MMSCF'
-    metric = 'k STM³'
-    metkgcm2 = 'k STM³'
-    metbar = 'k STM³'
+    metric = 'k STM3'
+    metkgcm2 = 'k STM3'
+    metbar = 'k STM3'
     lab = 'k stcc'
-    metric_atm = 'k STM³'
+    metric_atm = 'k STM3'
 
 
 class SurfaceVolumesLiquid(UnitDimension):
     """Units for surface volumes liquid."""
     english = 'MSTB'
-    metric = 'k STM³'
-    metkgcm2 = 'k STM³'
-    metbar = 'k STM³'
+    metric = 'k STM3'
+    metkgcm2 = 'k STM3'
+    metbar = 'k STM3'
     lab = 'k stcc'
-    metric_atm = 'k STM³'
+    metric_atm = 'k STM3'
 
 
 class Temperature(UnitDimension):
@@ -371,12 +371,12 @@ class TracerConcentrations(UnitDimension):
 
 class Transmissibility(UnitDimension):
     """Units for transmissibility."""
-    english = 'ft³·cp/day/psi'
-    metric = 'm³·cp/day/kPa'
-    metkgcm2 = 'm³·cp/day/kg/cm²'
-    metbar = 'm³·cp/day/bars'
+    english = 'ft3·cp/day/psi'
+    metric = 'm3·cp/day/kPa'
+    metkgcm2 = 'm3·cp/day/kg/cm2'
+    metbar = 'm3·cp/day/bars'
     lab = 'cc·cp/hour/psi'
-    metric_atm = 'm³·cp/day/atm'
+    metric_atm = 'm3·cp/day/atm'
 
 
 class Viscosity(UnitDimension):
@@ -391,40 +391,40 @@ class Viscosity(UnitDimension):
 
 class Volume(UnitDimension):
     """Units for volume."""
-    english = 'ft³'
-    metric = 'm³'
-    metkgcm2 = 'm³'
-    metbar = 'm³'
+    english = 'ft3'
+    metric = 'm3'
+    metkgcm2 = 'm3'
+    metbar = 'm3'
     lab = 'cc'
-    metric_atm = 'm³'
+    metric_atm = 'm3'
 
 
 class LiquidGasRatio(UnitDimension):
     """Liquid gas ratio units."""
     english = 'STB/MSCF'
-    metric = 'STM³/SM³'
-    metkgcm2 = 'STM³/SM³'
-    metbar = 'STM³/SM³'
+    metric = 'STM3/SM3'
+    metkgcm2 = 'STM3/SM3'
+    metbar = 'STM3/SM3'
     lab = 'stcc/stcc'
-    metric_atm = 'STM³/SM³'
+    metric_atm = 'STM3/SM3'
 
 
 class AttributeUnitDimension:
     """Class for handling the mapping between unit systems and the units used for that unit system."""
     attribute_map = {
-        'max_surface_oil_rate': SurfaceVolumesLiquid(),
-        'max_surface_water_rate': SurfaceVolumesLiquid(),
-        'max_surface_gas_rate': SurfaceVolumesGas(),
-        'max_surface_liquid_rate': SurfaceVolumesLiquid(),
+        'max_surface_oil_rate': SurfaceRatesLiquid(),
+        'max_surface_water_rate': SurfaceRatesLiquid(),
+        'max_surface_gas_rate': SurfaceRatesGas(),
+        'max_surface_liquid_rate': SurfaceRatesLiquid(),
         'max_reservoir_oil_rate': ReservoirRates(),
         'max_reservoir_water_rate': ReservoirRates(),
         'max_reservoir_gas_rate': ReservoirRates(),
         'max_reservoir_liquid_rate': ReservoirRates(),
         'max_hc_moles_rate': MolarRates(),
-        'max_reverse_surface_oil_rate': SurfaceVolumesLiquid(),
-        'max_reverse_surface_water_rate': SurfaceVolumesLiquid(),
-        'max_reverse_surface_gas_rate': SurfaceVolumesGas(),
-        'max_reverse_surface_liquid_rate': SurfaceVolumesLiquid(),
+        'max_reverse_surface_oil_rate': SurfaceRatesLiquid(),
+        'max_reverse_surface_water_rate': SurfaceRatesLiquid(),
+        'max_reverse_surface_gas_rate': SurfaceRatesGas(),
+        'max_reverse_surface_liquid_rate': SurfaceRatesLiquid(),
         'max_reverse_reservoir_oil_rate': ReservoirRates(),
         'max_reverse_reservoir_water_rate': ReservoirRates(),
         'max_reverse_reservoir_gas_rate': ReservoirRates(),
@@ -479,12 +479,12 @@ class AttributeUnitDimension:
         'max_cum_oil_prod': SurfaceVolumesLiquid(),
         'max_cum_water_prod': SurfaceVolumesLiquid(),
         'max_cum_liquid_prod': SurfaceVolumesLiquid(),
-        'qmult_oil_rate': SurfaceVolumesLiquid(),
-        'qmult_water_rate': SurfaceVolumesLiquid(),
-        'qmult_gas_rate': SurfaceVolumesGas(),
+        'qmult_oil_rate': SurfaceRatesLiquid(),
+        'qmult_water_rate': SurfaceRatesLiquid(),
+        'qmult_gas_rate': SurfaceRatesGas(),
     }
 
-    def get_unit(self, attribute_name: str, unit_system: UnitSystem, uppercase: bool = False) -> str:
+    def get_unit_from_attribute(self, attribute_name: str, unit_system: UnitSystem, uppercase: bool = False) -> str:
         """Returns the unit variable for the given unit system.
 
         Args:
--- tests/Units/test_unit_system_enum_to_variable.py
+++ tests/Units/test_unit_system_enum_to_variable.py
@@ -1,6 +1,7 @@
 import pytest
 from ResSimpy.Enums.UnitsEnum import UnitSystem
-from ResSimpy.Units.Units import Area
+from ResSimpy.Units.Units import Area, AttributeUnitDimension
+
 
 @pytest.mark.parametrize('unit_system, expected_result', [
     (UnitSystem.ENGLISH, 'ft2'),
@@ -18,3 +19,37 @@ def test_unit_system_enum_to_variable(unit_system, expected_result):
     result = unit_dimension.unit_system_enum_to_variable(unit_system=unit_system)
     # Assert
     assert result == expected_result
+
+@pytest.mark.parametrize('attribute, unit_system, expected_result', [
+    ('max_surface_water_rate', UnitSystem.ENGLISH, 'STB/day'),
+    ('max_lgr_perfplus', UnitSystem.METRIC, 'STM3/SM3'),
+    ('min_reverse_reservoir_water_rate', UnitSystem.LAB, 'cc/hour'),
+    ('max_surface_oil_rate', UnitSystem.METRIC_ATM, 'STM3/day'),
+    ('max_surface_gas_rate', UnitSystem.METBAR, 'SM3/day'),
+    ('max_surface_liquid_rate', UnitSystem.METKGCM2, 'STM3/day'),
+    ('max_reservoir_oil_rate', UnitSystem.METRIC, 'm3/day'),
+    ('max_reservoir_gas_rate', UnitSystem.METRIC, 'm3/day'),
+    ('min_pressure', UnitSystem.METRIC, 'kPa'),
+    ('max_pressure', UnitSystem.METRIC_ATM, 'atm'),
+    ('max_pressure', UnitSystem.METKGCM2, 'kg/cm2'),
+    ('min_temperature', UnitSystem.ENGLISH, 'degrees F'),
+])
+def test_get_unit(attribute, unit_system, expected_result):
+    """Tests the get_unit method."""
+    # Arrange
+    unit_dimension = AttributeUnitDimension()
+
+    # Act
+    result = unit_dimension.get_unit_from_attribute(unit_system=unit_system, attribute_name=attribute)
+    # Assert
+    assert result == expected_result
+
+def test_get_unit_error():
+    """Tests the get_unit method."""
+    # Arrange
+    unit_dimension = AttributeUnitDimension()
+
+    # Act
+    with pytest.raises(ValueError):
+        unit_dimension.get_unit_from_attribute(unit_system=UnitSystem.ENGLISH, attribute_name='not_an_attribute')
+
