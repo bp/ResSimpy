e60d3aaa69003414be0aabc62b78ae0c316b640c
jonnyh404 <121860375+jonnyh404@users.noreply.github.com>
2023-09-11 13:50:19 +0100

    remove units file

--- ResSimpy/Units/AttributeMappings/PropertyUnitsAttributeMapping.py
+++
@@ -1,8 +0,0 @@
-from typing import Mapping
-
-from ResSimpy.Units.AttributeMappings.AttributeMappingBase import AttributeMapBase
-from ResSimpy.Units.Units import UnitDimension
-
-
-class PropertyUnits(AttributeMapBase):
-    attribute_map: Mapping[str, UnitDimension] = {
