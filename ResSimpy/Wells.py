05a3747c325c44357f868254e550d5c8be28074e
Pranav Patil <pranavpatil@Pranavs-MacBook-Pro.local>
2023-09-18 17:21:11 +0530

    fixing linting and mypy issues

--- ResSimpy/Wells.py
+++ ResSimpy/Wells.py
@@ -1,8 +1,10 @@
 from __future__ import annotations
 from dataclasses import dataclass, field
 from abc import ABC
+from uuid import UUID
 
 import pandas as pd
+from ResSimpy.Enums.HowEnum import OperationEnum
 
 from ResSimpy.Well import Well
 from typing import Sequence, Optional
@@ -32,16 +34,23 @@ class Wells(ABC):
     def get_wells_overview(self) -> str:
         raise NotImplementedError("Implement this in the derived class")
 
-    def modify(self) -> str:
+    def modify(self, well_name: str, completion_properties_list: list[dict[str, None | float | int | str]],
+               how: OperationEnum = OperationEnum.ADD) -> None:
         raise NotImplementedError("Implement this in the derived class")
 
-    def add_completion(self) -> str:
+    def add_completion(self, well_name: str, completion_properties: dict[str, None | float | int | str],
+                       preserve_previous_completions: bool = True, comments: Optional[str] = None) -> None:
         raise NotImplementedError("Implement this in the derived class")
 
-    def remove_completion(self) -> str:
+    def remove_completion(self, well_name: str,
+                          completion_properties: Optional[dict[str, None | float | int | str]] = None,
+                          completion_id: Optional[UUID] = None) -> None:
         raise NotImplementedError("Implement this in the derived class")
 
-    def modify_completion(self) -> str:
+    def modify_completion(self, well_name: str, properties_to_modify: dict[str, None | float | int | str],
+                          completion_to_change: Optional[dict[str, None | float | int | str]] = None,
+                          completion_id: Optional[UUID] = None,
+                          comments: Optional[str] = None) -> None:
         raise NotImplementedError("Implement this in the derived class")
 
     @property
