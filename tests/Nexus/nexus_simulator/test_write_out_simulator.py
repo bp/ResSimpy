52f407bad20d850ff50e266f0b5c6736558c489e
jonnyh404 <121860375+jonnyh404@users.noreply.github.com>
2023-10-06 16:54:50 +0100

    adds test for relative paths in new location and updates the expected fcs

--- tests/Nexus/nexus_simulator/test_write_out_simulator.py
+++ tests/Nexus/nexus_simulator/test_write_out_simulator.py
@@ -119,10 +119,30 @@ class TestWriteOutSimulator:
         mocker.patch("builtins.open", writing_mock_open)
         # pretend a file has been modified
         nexus_sim.model_files.surface_files[1]._file_modified_set(True)
+
+        expected_runcontrol_path = '/nexus_data/runcontrol.dat'
+        expected_surface_path = os.path.join('/new_location', 'surface_file_01.dat')
+        expected_hyd_path = os.path.join('/path', 'hyd_method.dat')
+        expected_fcs_content = (
+            '\n'
+            '            RUN_UNITS ENGLISH\n'
+            '            DATEFORMAT DD/MM/YYYY\n'
+            '            RECURRENT_FILES\n'
+            f'            RUNCONTROL {expected_runcontrol_path}\n'
+            f'            SURFACE Network 1  {expected_surface_path}\n'
+            f'            HYD Method 1 {expected_hyd_path}\n'
+            '            ')
         # Act
-        nexus_sim.model_files.update_model_files(new_file_path='/new_location/new_file_name.fcs', new_include_file_location=None,
-                                              write_out_all_files=False, preserve_file_names=False,
-                                              overwrite_include_files=True)
+        nexus_sim.model_files.update_model_files(new_file_path='/new_location/new_file_name.fcs',
+                                                 new_include_file_location=None,
+                                                 write_out_all_files=False, preserve_file_names=False,
+                                                 overwrite_include_files=True)
         # assert
-        assert writing_mock_open.call_args_list[0][0][0] == '/surface_file_01.dat'
+        assert writing_mock_open.call_args_list[0][0][0] == expected_surface_path
         assert writing_mock_open.call_args_list[1][0][0] == '/new_location/new_file_name.fcs'
+
+        # Get all the calls to write() and check that the contents are what we expect
+        list_of_writes = [call for call in writing_mock_open.mock_calls if 'call().write' in str(call)]
+        assert len(list_of_writes) == 2
+        assert list_of_writes[0].args[0] == 'surface_file_content'
+        assert list_of_writes[1].args[0] == expected_fcs_content
