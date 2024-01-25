b04e867c5dd923b73632cd7802c0c1181b78cb5e
jonnyh404 <121860375+jonnyh404@users.noreply.github.com>
2024-01-25 17:11:42 +0000

    set up initial tests for reading in output requests

--- ResSimpy/Nexus/NexusReporting.py
+++ ResSimpy/Nexus/NexusReporting.py
@@ -1,9 +1,28 @@
 """Class for handling all Reporting and runcontrol related tasks."""
+import warnings
+from dataclasses import dataclass
+
 import ResSimpy.Nexus.nexus_file_operations as nfo
 import ResSimpy.FileOperations.file_operations as fo
+from ResSimpy.Enums.FrequencyEnum import FrequencyEnum
+from ResSimpy.Enums.OutputType import OutputType
 
 
-class Reporting:
+@dataclass(kw_only=True)
+class NexusOutputRequest:
+    """Class for handling output requests in Nexus."""
+    date: str
+    output: str
+    output_type: OutputType
+    output_frequency: FrequencyEnum
+    output_frequency_number: None | float
+
+
+@dataclass(kw_only=True)
+class NexusReporting:
+    """Class for handling all Reporting and runcontrol related tasks."""
+    ss_output_requests: list[NexusOutputRequest]
+    array_output_requests: list[NexusOutputRequest]
     def __init__(self, model) -> None:
         self.__model = model
 
@@ -44,3 +63,72 @@ class Reporting:
         new_file_str = "".join(new_file)
         with open(structured_grid_path, "w") as text_file:
             text_file.write(new_file_str)
+    
+    def load_output_requests(self):
+        """Loads output requests from the Nexus runcontrol file."""
+        if self.__model.model_files.runcontrol_file is None:
+            raise ValueError("No file path given or found for runcontrol file path. \
+                Please update runcontrol file path")
+        file_as_list = self.__model.model_files.runcontrol_file.get_flat_list_str_file
+
+        # Get the output requests
+        ss_output_requests = []
+        array_output_requests = []
+        ss_start_index: int = -1
+        ss_end_index: int = -1
+        array_start_index: int = -1
+        array_end_index: int = -1
+        current_date = self.__model.start_date
+        for index, line in enumerate(file_as_list):
+            if nfo.check_token('SPREADSHEET', line):
+                 ss_start_index = index+1
+
+            if ss_start_index > -1 and nfo.check_token('ENDSPREADSHEET', line):
+                ss_end_index = index
+                list_of_output_requests = self.__get_output_request(file_as_list[ss_start_index:ss_end_index],
+                                                                    date=current_date,
+                                                                    output_type=OutputType.SPREADSHEET)
+                ss_output_requests.extend()
+
+            if nfo.check_token('OUTPUT', line):
+                array_start_index = index+1
+            if array_start_index > -1 and nfo.check_token('ENDOUTPUT', line):
+                array_end_index = index+1
+                list_of_output_requests = self.__get_output_request(file_as_list[array_start_index:array_end_index],
+                                                                    date=current_date,
+                                                                    output_type=OutputType.ARRAY)
+                array_output_requests.extend(list_of_output_requests)
+
+
+        self.ss_output_requests = ss_output_requests
+        self.array_output_requests = array_output_requests
+
+    @staticmethod
+    def _get_output_request(table_file_as_list: list[str], date: str, output_type: OutputType)\
+            -> list[NexusOutputRequest]:
+        """Gets the output objects from the runcontrol file."""
+        output_request_with_number = ['DT', 'DTTOL', 'FREQ', 'TIMESTEP']
+
+        resulting_output_requests: list[NexusOutputRequest] = []
+        for line in table_file_as_list:
+            number_defined_float: None | float = None
+            element = nfo.get_next_value(start_line_index=0, file_as_list=[line])
+            if element is None or element == '':
+                continue
+            frequency = nfo.get_expected_token_value(element, token_line=line,
+                                                     file_list=[line])
+            if frequency in output_request_with_number:
+                number_defined = nfo.get_expected_token_value(frequency, token_line=line,
+                                                              file_list=[line])
+                try:
+                    number_defined_float = float(number_defined)
+                except ValueError:
+                    warnings.warn(f'Unable to convert output request number to float: {number_defined}')
+                    number_defined_float = None
+
+            frequency_enum = FrequencyEnum[frequency]
+            output_request = NexusOutputRequest(date=date, output=element, output_frequency=frequency_enum,
+                                                output_frequency_number=number_defined_float, output_type=output_type)
+            resulting_output_requests.append(output_request)
+
+        return resulting_output_requests
