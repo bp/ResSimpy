c94c68a1e5bfcdf88eca5772d6ee122bd1495065
Keith Robertson <69997096+kwr3@users.noreply.github.com>
2024-02-13 12:58:07 -0600

    Updating NexusProc class (#277)
    * Searches for procedures in the surface file.
    
    * Searches for procedures in the surface file.
    
    * Searches for procedures in the surface file.
    
    * Searches for procedures in the surface file.
    
    * This adds property methods to the NexusProc class.
    
    * This adds property methods to the NexusProc class.
    
    * This adds property methods to the NexusProc class.

--- ResSimpy/Nexus/DataModels/Network/NexusProc.py
+++ ResSimpy/Nexus/DataModels/Network/NexusProc.py
@@ -17,11 +17,31 @@ class NexusProc:
         self.__priority = priority
         self.__contents = contents
 
+    @property
+    def contents(self) -> list[str]:
+        """Returns the contents of the main body of the procedure."""
+        return self.__contents
+
+    @property
+    def date(self) -> str:
+        """Returns the date that the procedure occurred."""
+        return self.__date
+
+    @property
+    def priority(self) -> Optional[int]:
+        """Returns the priority of the procedure."""
+        return self.__priority
+
+    @property
+    def name(self) -> Optional[str]:
+        """Returns the name of the procedure."""
+        return self.__name
+
     @staticmethod
     def get_keyword_mapping() -> Mapping[str, tuple[str, type]]:
         """Gets the mapping of nexus keywords to attribute definitions."""
         keywords = {
             'NAME': ('name', str),
             'PRIORITY': ('priority', str)
-            }
+        }
         return keywords
