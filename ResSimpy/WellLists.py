8f60249bbcff1a2dc8746315104c3a1e4195777b
jonnyh404 <121860375+jonnyh404@users.noreply.github.com>
2024-03-28 16:55:52 +0000

    adds well list base class attributes and adds as attribute to Network base class

--- ResSimpy/WellLists.py
+++ ResSimpy/WellLists.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
-from abc import ABC
-from typing import Literal
+from abc import ABC, abstractmethod
+from typing import Literal, Sequence
 
 
 @dataclass(kw_only=True)
@@ -14,7 +14,26 @@ class WellList:
 @dataclass(kw_only=True)
 class WellLists(ABC):
     """Class for representing a set of WellList objects for the model."""
+    __well_lists: Sequence[WellList]
+
+    def __init__(self, welllists: Sequence[WellList] = None):
+        """Initialises the WellLists class.
+
+        Args:
+            welllists (Sequence[WellList]): List of WellList objects.
+            """
+        self.__well_lists = welllists
 
     @property
     def _network_element_name(self) -> Literal['welllists']:
         return 'welllists'
+
+    @property
+    def welllists(self) -> Sequence[WellList]:
+        """Returns all WellList instances."""
+        return self.__well_lists
+
+    @property
+    def unique_names(self) -> list[str]:
+        """Returns all WellList names."""
+        return list({x.name for x in self.welllists})
