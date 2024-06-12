2cafd946c98978f8250cdb6c9f9ec5c356eb415d
Daniel Barclay <daniel.barclay@bp.com>
2024-06-12 11:22:31 +0100

    add test cases for mmddyy format

--- tests/Nexus/nexus_simulator/test_nexus_network.py
+++ tests/Nexus/nexus_simulator/test_nexus_network.py
@@ -585,7 +585,7 @@ def test_load_wellbore(mocker, file_contents, wellboreprops1, wellboreprops2):
 
 @pytest.mark.parametrize('file_contents, wellboreprops1, wellboreprops2', [
     (''' TIME 01/03/2019
-         TIME PLUS 4
+TIME PLUS 4
 WELLBORE
 WELL METHOD DIAM TYPE
 well1 BEGGS 3.5 PIPE
@@ -598,7 +598,22 @@ ENDWELLBORE
      {'name': 'well1', 'bore_type': 'PIPE', 'hyd_method': "BEGGS", 'diameter': 3.5, 'date': '05/03/2019',
       'unit_system': UnitSystem.ENGLISH},
      {'name': 'well2', 'hyd_method': "BRILL", 'diameter': 3.25, 'flowsect': 2, 'roughness': 0.2002,
-      'date': '05/03/2019', 'unit_system': UnitSystem.ENGLISH},)
+      'date': '05/03/2019', 'unit_system': UnitSystem.ENGLISH},),
+    (''' TIME 25/08/2019
+TIME PLUS 2.9
+WELLBORE
+WELL METHOD DIAM TYPE
+well1 BEGGS 3.5 PIPE
+ENDWELLBORE
+WELLBORE
+WELL METHOD DIAM FLOWSECT ROUGHNESS
+well2 BRILL 3.25 2      0.2002
+ENDWELLBORE
+    ''',
+     {'name': 'well1', 'bore_type': 'PIPE', 'hyd_method': "BEGGS", 'diameter': 3.5, 'date': '27/08/2019(21:36:00)',
+      'unit_system': UnitSystem.ENGLISH},
+     {'name': 'well2', 'hyd_method': "BRILL", 'diameter': 3.25, 'flowsect': 2, 'roughness': 0.2002,
+      'date': '27/08/2019(21:36:00)', 'unit_system': UnitSystem.ENGLISH},)
 ])
 def test_load_wellbore_time_plus(mocker, file_contents, wellboreprops1, wellboreprops2):
     # Arrange
@@ -611,6 +626,65 @@ def test_load_wellbore_time_plus(mocker, file_contents, wellboreprops1, wellbore
     expected_result = [wellbore1, wellbore2]
     mock_nexus_network = mocker.MagicMock()
     mock_nexus_network.model.date_format = DateFormat.DD_MM_YYYY
+    mocker.patch('ResSimpy.Nexus.NexusNetwork.NexusNetwork', mock_nexus_network)
+    nexuswellbore = NexusWellbores(mock_nexus_network)
+    expected_df = pd.DataFrame([wellboreprops1, wellboreprops2])
+    expected_df = expected_df.fillna(value=np.nan).dropna(axis=1, how='all')
+
+    # Act
+    nexuswellbore.load(surface_file, start_date, default_units=UnitSystem.ENGLISH)
+    result = nexuswellbore.get_all()
+    single_wellbore = nexuswellbore.get_by_name('well1')
+    result_df = nexuswellbore.get_df()
+
+    # Assert
+    assert result == expected_result
+    assert single_wellbore == wellbore1
+    pd.testing.assert_frame_equal(result_df, expected_df, check_like=True)
+
+@pytest.mark.parametrize('file_contents, wellboreprops1, wellboreprops2', [
+    (''' TIME 12/28/2019
+TIME PLUS 6
+WELLBORE
+WELL METHOD DIAM TYPE
+well1 BEGGS 3.5 PIPE
+ENDWELLBORE
+WELLBORE
+WELL METHOD DIAM FLOWSECT ROUGHNESS
+well2 BRILL 3.25 2      0.2002
+ENDWELLBORE
+    ''',
+     {'name': 'well1', 'bore_type': 'PIPE', 'hyd_method': "BEGGS", 'diameter': 3.5, 'date': '01/03/2020',
+      'unit_system': UnitSystem.ENGLISH},
+     {'name': 'well2', 'hyd_method': "BRILL", 'diameter': 3.25, 'flowsect': 2, 'roughness': 0.2002,
+      'date': '01/03/2020', 'unit_system': UnitSystem.ENGLISH},),
+    (''' TIME 05/11/2019
+TIME PLUS 1.1
+WELLBORE
+WELL METHOD DIAM TYPE
+well1 BEGGS 3.5 PIPE
+ENDWELLBORE
+WELLBORE
+WELL METHOD DIAM FLOWSECT ROUGHNESS
+well2 BRILL 3.25 2      0.2002
+ENDWELLBORE
+    ''',
+     {'name': 'well1', 'bore_type': 'PIPE', 'hyd_method': "BEGGS", 'diameter': 3.5, 'date': '05/12/2019(02:24:00)',
+      'unit_system': UnitSystem.ENGLISH},
+     {'name': 'well2', 'hyd_method': "BRILL", 'diameter': 3.25, 'flowsect': 2, 'roughness': 0.2002,
+      'date': '05/12/2019(02:24:00)', 'unit_system': UnitSystem.ENGLISH},)
+])
+def test_load_wellbore_time_plus_mmddyy(mocker, file_contents, wellboreprops1, wellboreprops2):
+    # Arrange
+    mocker.patch('ResSimpy.DataObjectMixin.uuid4', return_value='uuid_1')
+    start_date = '01/01/2018'
+    surface_file = NexusFile(location='surface.dat', file_content_as_list=file_contents.splitlines())
+
+    wellbore1 = NexusWellbore(wellboreprops1, date_format=DateFormat.MM_DD_YYYY)
+    wellbore2 = NexusWellbore(wellboreprops2, date_format=DateFormat.MM_DD_YYYY)
+    expected_result = [wellbore1, wellbore2]
+    mock_nexus_network = mocker.MagicMock()
+    mock_nexus_network.model.date_format = DateFormat.MM_DD_YYYY
     mocker.patch('ResSimpy.Nexus.NexusNetwork.NexusNetwork', mock_nexus_network)
     nexuswellbore = NexusWellbores(mock_nexus_network)
     expected_df = pd.DataFrame([wellboreprops1, wellboreprops2])
