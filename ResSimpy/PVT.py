65db14ce5f9226e7aeedf225bba3aaa6d5a6b2df
Mahmoud Alamar <mahmoud.alamar@bp.com>
2024-09-13 11:02:31 -0500

    Fixing spacing

--- ResSimpy/PVT.py
+++ ResSimpy/PVT.py
@@ -13,7 +13,6 @@ class PVT(ABC):
         inputs (dict[int, DynamicProperty]): Collection of PVT inputs, as a dictionary.
     """
 
-
     @property
     def inputs(self) -> Mapping[int, DynamicProperty]:
         """A Collection of PVT inputs, as a dictionary."""
