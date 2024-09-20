6219871cc0ff252ad5548e89bda90c215cb08bbd
Chris Flynn <Chris@Flynnsoftware.com>
2024-09-20 14:25:45 +0100

    Fixing Node.

--- ResSimpy/Node.py
+++ ResSimpy/Node.py
@@ -17,7 +17,7 @@ class Node(NetworkObject, ABC):
 
     def __init__(self, properties_dict: Optional[dict[str, None | int | str | float]] = None,
                  date: Optional[str] = None, date_format: Optional[DateFormat] = None, start_date: Optional[str] = None,
-                 unit_system: Optional[UnitSystem] = None) -> None:
+                 unit_system: Optional[UnitSystem] = None, name: Optional[str] = None) -> None:
         """Initialises the NexusNode class.
 
         Args:
@@ -26,6 +26,7 @@ class Node(NetworkObject, ABC):
             date_format (Optional[DateFormat]): The date format that the object uses.
             start_date (Optional[str]): The start date of the model. Required if the object uses a decimal TIME.
             unit_system (Optional[UnitSystem]): The unit system of the object e.g. ENGLISH, METRIC.
+            name (Optional[str]): The name of the object.
         """
         super().__init__(properties_dict=properties_dict, date=date, date_format=date_format, start_date=start_date,
                          unit_system=unit_system)
