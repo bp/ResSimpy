733ccfe9242cace08617dcd3dda74f41977f9728
Jonny Hall <121860375+jonnyh404@users.noreply.github.com>
2024-09-23 14:03:13 +0100

    File split long lines (#392)
    * failing tests
    
    * functions to split file as list into multi line strings
    
    * test for multiline string
    
    * flake8
    
    * move whitespace import

--- tests/FileOperations/test_file_operations.py
+++ tests/FileOperations/test_file_operations.py
@@ -120,3 +120,34 @@ def test_get_nth_value(list_of_strings, value_number_to_get, expected_result):
 
     # Assert
     assert result == expected_result
+
+
+@pytest.mark.parametrize("long_string, expected_result, max_length", [
+    ('This is a long string that needs to be split into multiple lines based on a certain length.\n',
+     'This is a long string that needs to be split into multiple lines\nbased on a certain length.\n', 68),
+    ('This is a long string that needs to be split into multiple lines based on a certain length.\n',
+     'This is a long string that needs to be split into multiple lines based on a certain length.\n', 100),
+    ('This is a long string that needs to be split into multiple lines based on a certain length.\n',
+     'This is a long\nstring that needs to\nbe split into\nmultiple lines based\non a certain length.\n', 20),])
+def test_split_lines_for_long_string(long_string, expected_result, max_length):
+    # Arrange
+    # Act
+    result = fo.split_lines_for_long_string(long_string, max_length=max_length)
+    
+    # Assert
+    assert result == expected_result
+
+def test_split_list_of_strings_by_length():
+    # Arrange
+    list_of_strings = ['This is a long string that\n', 'needs to be split into multiple lines\n']
+    expected_result = [
+        'This is a long\nstring that\n',
+        'needs to be split\ninto multiple lines\n'
+    ]
+    max_length = 20
+
+    # Act
+    result = fo.split_list_of_strings_by_length(list_of_strings, max_length)
+
+    # Assert
+    assert result == expected_result
