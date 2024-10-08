1904febda32e3b86bc01d433759eef69bb40303f
Mahmoud Alamar <mahmoud.alamar@bp.com>
2024-10-08 13:24:00 -0500

    Converting propreties_dict into named parameters.

--- ResSimpy/Target.py
+++ ResSimpy/Target.py
@@ -33,7 +33,16 @@ class Target(NetworkObject, ABC):
 
     def __init__(self, properties_dict: Optional[dict[str, None | int | str | float]] = None,
                  date: Optional[str] = None, date_format: Optional[DateFormat] = None, start_date: Optional[str] = None,
-                 unit_system: Optional[UnitSystem] = None, name: Optional[str] = None) -> None:
+                 unit_system: Optional[UnitSystem] = None, name: Optional[str] = None,
+                 control_quantity: Optional[str] = None, control_conditions: Optional[str] = None,
+                 control_connections: Optional[str] = None, control_method: Optional[str] = None,
+                 calculation_method: Optional[str] = None, calculation_conditions: Optional[str] = None,
+                 calculation_connections: Optional[str] = None, value: Optional[float] = None,
+                 add_value: Optional[float] = None, region: Optional[str] = None, priority: Optional[int] = None,
+                 minimum_rate: Optional[str] = None, minimum_rate_no_shut: Optional[float] = None,
+                 guide_rate: Optional[str] = None, max_change_pressure: Optional[float] = None,
+                 rank_dt: Optional[float] = None, control_type: Optional[str] = None,
+                 calculation_type: Optional[str] = None, region_number: Optional[int] = None) -> None:
         """Initialises the Target class.
 
         Args:
@@ -43,7 +52,28 @@ class Target(NetworkObject, ABC):
             start_date (Optional[str]): The start date of the model. Required if the object uses a decimal TIME.
             unit_system (Optional[UnitSystem]): The unit system of the object e.g. ENGLISH, METRIC.
             name (Optional[str]): The name of the object.
+            control_quantity(Optional[str]):
         """
+        self.control_quantity = None
+        self.control_conditions = None
+        self.control_connections = None
+        self.control_method = None
+        self.calculation_method = None
+        self.calculation_conditions = None
+        self.calculation_connections = None
+        self.value = None
+        self.add_value = None
+        self.region = None
+        self.priority = None
+        self.minimum_rate = None
+        self.minimum_rate_no_shut = None
+        self.guide_rate = None
+        self.max_change_pressure = None
+        self.rank_dt = None
+        self.control_type = None
+        self.calculation_type = None
+        self.region_number = None
+
         super().__init__(properties_dict=properties_dict, date=date, date_format=date_format, start_date=start_date,
                          unit_system=unit_system, name=name)
 
