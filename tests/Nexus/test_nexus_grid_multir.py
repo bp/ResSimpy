0a621587adc4a7af1717a093825ba50fb8d9c4fa
jonnyh404 <121860375+jonnyh404@users.noreply.github.com>
2024-10-23 14:33:16 +0100

    fixes multir by making it an object and enables arbitrary length of line

--- tests/Nexus/test_nexus_grid_multir.py
+++ tests/Nexus/test_nexus_grid_multir.py
@@ -1,8 +1,10 @@
 import pandas as pd
 import pytest
 
+from ResSimpy.DataModelBaseClasses.Multir import Multir
 from ResSimpy.Nexus.DataModels.NexusFile import NexusFile
 from ResSimpy.Nexus.DataModels.StructuredGrid.NexusGrid import NexusGrid
+from ResSimpy.Nexus.DataModels.StructuredGrid.NexusMultir import NexusMultir
 
 
 @pytest.mark.parametrize("file_content, expected_result", [
@@ -11,55 +13,50 @@ from ResSimpy.Nexus.DataModels.StructuredGrid.NexusGrid import NexusGrid
     
     MULTIR
     
-    1	2	0.1	XYZ	ALL
-    1	3	0	XYZ	ALL
-    1	4	0	XYZ	ALL
+    1	2	0.1	X Y Z	ALL
+    1	3	0	Y Z
+    1	4	0	XYZ	STD
     
-    1	5	0	XYZ	ALL
-    1	6	0	XYZ	ALL""", pd.DataFrame({'region_1': [1, 1, 1, 1, 1],
-                       'region_2': [2, 3, 4, 5, 6],
-                       'tmult': [0.1, 0, 0, 0, 0],
-                       'directions': ['XYZ', 'XYZ', 'XYZ', 'XYZ', 'XYZ'],
-                       'connections': ['ALL', 'ALL', 'ALL', 'ALL', 'ALL']})),
+    1	5	0.24	XYZ	NONSTD
+    1	6	0	XYZ	ALL""",
+     [NexusMultir(region_1=1, region_2=2, tmult=0.1, directions='XYZ', std_connections=True, non_std_connections=True),
+      NexusMultir(region_1=1, region_2=3, tmult=0.0, directions='YZ', std_connections=True, non_std_connections=True),
+      NexusMultir(region_1=1, region_2=4, tmult=0.0, directions='XYZ', std_connections=True, non_std_connections=False),
+      NexusMultir(region_1=1, region_2=5, tmult=0.24, directions='XYZ', std_connections=False, non_std_connections=True),
+      NexusMultir(region_1=1, region_2=6, tmult=0.0, directions='XYZ', std_connections=True, non_std_connections=True)]),
     # Empty MULTIR table
     ("""
     MULTIR
     """,
-     pd.DataFrame(columns=['region_1', 'region_2', 'tmult', 'directions', 'connections']).astype({'region_1': 'int',
-                                                'region_2': 'int',
-                                                'tmult': 'float64'})),
+     []),
 
     # Test with a keyword after MULTIR
     ("""KX CON 
      5.1
      
      MULTIR
-     2 2 3.1 X StD
+     2 2 3.1 X StD  ! comment after
      2 4 5 XYZ ALL
      KZ CON
      5.1
      """,
-     pd.DataFrame({'region_1': [2, 2],
-                   'region_2': [2, 4],
-                   'tmult': [3.1, 5],
-                   'directions': ['X', 'XYZ'],
-                   'connections': ['StD', 'ALL']})),
-    
+     [NexusMultir(region_1=2, region_2=2, tmult=3.1, directions='X', std_connections=True, non_std_connections=False),
+      NexusMultir(region_1=2, region_2=4, tmult=5, directions='XYZ', std_connections=True, non_std_connections=True)]),
     # multiple MULTIR tables
     ("""MULTIR
     1 2 3 X StD
-    1 4 5 XYZ ALL
+    1 4 5 XY Z ALL
     
     
     MULTIR
     2 2 3 X StD
+    ! comment line
     2 4 5.5 XYZ ALL
     """,
-     pd.DataFrame({'region_1': [1, 1, 2, 2],
-                   'region_2': [2, 4, 2, 4],
-                   'tmult': [3, 5, 3, 5.5],
-                   'directions': ['X', 'XYZ', 'X', 'XYZ'],
-                   'connections': ['StD', 'ALL', 'StD', 'ALL']})),
+     [NexusMultir(region_1=1, region_2=2, tmult=3, directions='X', std_connections=True, non_std_connections=False),
+      NexusMultir(region_1=1, region_2=4, tmult=5, directions='XYZ', std_connections=True, non_std_connections=True),
+      NexusMultir(region_1=2, region_2=2, tmult=3, directions='X', std_connections=True, non_std_connections=False),
+      NexusMultir(region_1=2, region_2=4, tmult=5.5, directions='XYZ', std_connections=True, non_std_connections=True)])
 ]
     , ids=['basic', 'empty', 'stop_on_keyword', 'multiple_tables'])
 def test_load_nexus_multir_table_from_list(file_content, expected_result):
@@ -69,7 +66,8 @@ def test_load_nexus_multir_table_from_list(file_content, expected_result):
     result = NexusGrid.load_nexus_multir_table_from_list(file_content_as_list)
 
     # Assert
-    pd.testing.assert_frame_equal(result, expected_result)
+    assert result == expected_result
+
 
 def test_load_multir():
     # Arrange
@@ -87,14 +85,16 @@ def test_load_multir():
 
     grid = NexusGrid(grid_nexus_file=nexus_grid_file)
     grid._nexus_file_content = file_content
+    
+    expected_result = [NexusMultir(region_1=1, region_2=2, tmult=0, directions='XYZ', std_connections=True, non_std_connections=True),
+                       NexusMultir(region_1=1, region_2=3, tmult=0, directions='XYZ', std_connections=True, non_std_connections=True),
+                       NexusMultir(region_1=1, region_2=4, tmult=0.1, directions='XYZ', std_connections=True, non_std_connections=True),
+                       NexusMultir(region_1=1, region_2=5, tmult=0, directions='XYZ', std_connections=True, non_std_connections=True),
+                       NexusMultir(region_1=1, region_2=6, tmult=0, directions='XYZ', std_connections=True, non_std_connections=True)]
 
+    
     # Act
     result = grid.multir
 
     # Assert
-    expected_result = pd.DataFrame({'region_1': [1, 1, 1, 1, 1],
-                                    'region_2': [2, 3, 4, 5, 6],
-                                    'tmult': [0, 0, 0.1, 0, 0],
-                                    'directions': ['XYZ', 'XYZ', 'XYZ', 'XYZ', 'XYZ'],
-                                    'connections': ['ALL', 'ALL', 'ALL', 'ALL', 'ALL']})
-    pd.testing.assert_frame_equal(result, expected_result)
+    assert result == expected_result
