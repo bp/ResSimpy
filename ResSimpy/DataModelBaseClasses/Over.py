8265398a159341a9e62c3cab0d69fd513afed0c2
jonnyh404 <121860375+jonnyh404@users.noreply.github.com>
2024-10-28 16:09:25 +0000

    Adds the base class for OVERride keywords

---
+++ ResSimpy/DataModelBaseClasses/Over.py
@@ -0,0 +1,33 @@
+from abc import ABC
+from dataclasses import dataclass
+from typing import Optional
+
+
+@dataclass
+class Over(ABC):
+    """Used to represent an abstract base of Overrides for transmissibility and pore volumes.
+
+    Attributes:
+        arrays (list[str]): The arrays to be modified by the over.
+        i1 (int): The start of the i range.
+        i2 (int): The end of the i range.
+        j1 (int): The start of the j range.
+        j2 (int): The end of the j range.
+        k1 (int): The start of the k range.
+        k2 (int): The end of the k range.
+        operator (str): The operator to be assigned to the range.
+        value (float): The value to be assigned to the range.
+        threshold (float): The value for which GE or LE is used.
+    """
+    arrays: list[str]
+    i1: int
+    i2: int
+    j1: int
+    j2: int
+    k1: int
+    k2: int
+    operator: str
+    value: float
+    threshold: Optional[float] = None
+    grid: Optional[str] = None
+    fault_name: Optional[str] = None
