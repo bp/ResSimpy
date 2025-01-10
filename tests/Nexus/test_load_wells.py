2c6b8903fcfb50f21f2565e72033a8862fd484e8
MahmoudAlamar03 <hamadaalzain88@gmail.com>
2025-01-10 10:58:08 -0600

    Cleaning sensitive data

--- tests/Nexus/test_load_wells.py
+++ tests/Nexus/test_load_wells.py
@@ -1222,7 +1222,7 @@ WELLS set 1 wells.dat"""
     assert wells[1] == expected_result_2
 
 
-def test_load_wells_maddog(mocker):
+def test_load_wells_bug_1(mocker):
     # Arrange
     start_date = '01/12/2005'
     expected_date = '01/12/2005'
@@ -1230,16 +1230,16 @@ def test_load_wells_maddog(mocker):
     mocker.patch('ResSimpy.DataModelBaseClasses.DataObjectMixin.uuid4', return_value='uuid1')
 
     wellspec_contents = f"""
-WELLSPEC MD224
+WELLSPEC Well1
         IW      JW      L     GRID    ANGLA   ANGLV   LENGTH  RADW    STAT    perm_thickness_ovr    SKIN    !    DEPTH   MD
         153     95      9     ROOT    298.192 60.4133 12.745  0.35    ON      3752.773908        -1.6    !    21694.6 22799.8
         
 ! Six SWP3 locations
 ! INCLUDE Well_SWP3_loc6.inc
 
-WELLSPEC        SP6     !FINAL_zMD_DC1_SP1_B2_JLPP      MD2
-! Glen A.: shifted well location slightly to move away from new OBN fault
-! Glen A.: swapped with SP1 location - Feb 2021
+WELLSPEC        Well2     !Comment      Well1
+! First comment.: first comment
+! Second comment.: second comment
         IW      JW      L       GRID    ANGLA   ANGLV   LENGTH  RADW    STAT    perm_thickness_ovr      SKIN
         163     126     9       ROOT    108.345 38.5919 11.8571 0.35    ON      0.786908177144367       0.0
         163     126     10      ROOT    108.274 38.5919 11.8544 0.35    ON      0.786908177144367       0.0
