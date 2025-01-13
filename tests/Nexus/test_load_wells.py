4c168de47688c659b581708d47853c9b3776fac5
jonnyh404 <121860375+jonnyh404@users.noreply.github.com>
2025-01-13 17:14:15 +0000

    sanitize

--- tests/Nexus/test_load_wells.py
+++ tests/Nexus/test_load_wells.py
@@ -1234,8 +1234,8 @@ WELLSPEC Well1
         IW      JW      L     GRID    ANGLA   ANGLV   LENGTH  RADW    STAT    perm_thickness_ovr    SKIN    !    DEPTH   MD
         153     95      9     ROOT    298.192 60.4133 12.745  0.35    ON      3752.773908        -1.6    !    21694.6 22799.8
         
-! Six SWP3 locations
-! INCLUDE Well_SWP3_loc6.inc
+! ADDITIONAL COMMENT
+! INCLUDE wells.inc
 
 WELLSPEC        Well2     !Comment      Well1
 ! First comment.: first comment
