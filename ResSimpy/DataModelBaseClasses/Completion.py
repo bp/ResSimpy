7cc5d915ae3664ed26224028c65579e4f5389427
Chris Flynn <Chris@Flynnsoftware.com>
2025-03-27 15:55:37 +0000

    Setting inherited properties in `Completion` to protected rather than private.

--- ResSimpy/DataModelBaseClasses/Completion.py
+++ ResSimpy/DataModelBaseClasses/Completion.py
@@ -43,26 +43,25 @@ class Completion(DataObjectMixin, ABC):
         peaceman_well_block_radius (Optional[float]): The pressure equivalent radius of the grid block
     """
 
-    __i: Optional[int] = None
-    __j: Optional[int] = None
-    __k: Optional[int] = None
-    __skin: Optional[float] = None
-    __depth: Optional[float] = None
-    __well_radius: Optional[float] = None
-    __x: Optional[float] = None
-    __y: Optional[float] = None
-    __angle_a: Optional[float] = None
-    __angle_v: Optional[float] = None
-    __grid: Optional[str] = None
-    __depth_to_top: Optional[float] = None
-    __depth_to_bottom: Optional[float] = None
-    __perm_thickness_ovr: Optional[float] = None
-    __dfactor: Optional[float] = None
-    __rel_perm_method: Optional[int] = None
-    __status: Optional[str] = None
-    __unit_system: Optional[UnitSystem] = None
-    __peaceman_well_block_radius: Optional[float] = None
-    __perm_thickness_mult: Optional[float] = None
+    _i: Optional[int] = None
+    _j: Optional[int] = None
+    _k: Optional[int] = None
+    _skin: Optional[float] = None
+    _depth: Optional[float] = None
+    _well_radius: Optional[float] = None
+    _x: Optional[float] = None
+    _y: Optional[float] = None
+    _angle_a: Optional[float] = None
+    _angle_v: Optional[float] = None
+    _grid: Optional[str] = None
+    _depth_to_top: Optional[float] = None
+    _depth_to_bottom: Optional[float] = None
+    _perm_thickness_ovr: Optional[float] = None
+    _dfactor: Optional[float] = None
+    _rel_perm_method: Optional[int] = None
+    _status: Optional[str] = None
+    _peaceman_well_block_radius: Optional[float] = None
+    _perm_thickness_mult: Optional[float] = None
 
     def __init__(self, date: str, i: Optional[int] = None, j: Optional[int] = None, k: Optional[int] = None,
                  skin: Optional[float] = None, depth: Optional[float] = None, well_radius: Optional[float] = None,
@@ -103,121 +102,121 @@ class Completion(DataObjectMixin, ABC):
             unit_system: Optional[UnitSystem]: The unit system to use for the completion.
         """
         super().__init__(date=date, date_format=date_format, start_date=start_date, unit_system=unit_system)
-        self.__well_radius = well_radius
-        self.__i = i
-        self.__j = j
-        self.__k = k
-        self.__skin = skin
-        self.__depth = depth
-        self.__x = x
-        self.__y = y
-        self.__angle_a = angle_a
-        self.__angle_v = angle_v
-        self.__grid = grid
-        self.__depth_to_top = depth_to_top
-        self.__depth_to_bottom = depth_to_bottom
-        self.__perm_thickness_ovr = perm_thickness_ovr
-        self.__dfactor = dfactor
-        self.__rel_perm_method = rel_perm_method
-        self.__perm_thickness_mult = perm_thickness_mult
-        self.__status = status
+        self._well_radius = well_radius
+        self._i = i
+        self._j = j
+        self._k = k
+        self._skin = skin
+        self._depth = depth
+        self._x = x
+        self._y = y
+        self._angle_a = angle_a
+        self._angle_v = angle_v
+        self._grid = grid
+        self._depth_to_top = depth_to_top
+        self._depth_to_bottom = depth_to_bottom
+        self._perm_thickness_ovr = perm_thickness_ovr
+        self._dfactor = dfactor
+        self._rel_perm_method = rel_perm_method
+        self._perm_thickness_mult = perm_thickness_mult
+        self._status = status
         self.__unit_system = unit_system
-        self.__peaceman_well_block_radius = peaceman_well_block_radius
+        self._peaceman_well_block_radius = peaceman_well_block_radius
 
     @property
     def well_radius(self) -> float | None:
         """Returns well radius as float."""
-        return self.__well_radius
+        return self._well_radius
 
     @property
     def i(self) -> int | None:
         """Returns grid cell location in the x direction as an int."""
-        return self.__i
+        return self._i
 
     @property
     def j(self) -> int | None:
         """Returns grid cell location in the y direction as an int."""
-        return self.__j
+        return self._j
 
     @property
     def k(self) -> int | None:
         """Returns grid cell location in the z direction as an int."""
-        return self.__k
+        return self._k
 
     @property
     def skin(self) -> float | None:
         """Returns skin value of the completion as a float."""
-        return self.__skin
+        return self._skin
 
     @property
     def depth(self) -> float | None:
         """Returns the depth of the completion as a float."""
-        return self.__depth
+        return self._depth
 
     @property
     def x(self) -> float | None:
         """Returns the location of the completion in the x direction."""
-        return self.__x
+        return self._x
 
     @property
     def y(self) -> float | None:
         """Returns the location of the completion in the y direction."""
-        return self.__y
+        return self._y
 
     @property
     def angle_a(self) -> float | None:
         """Returns the angle relative to the local I axis."""
-        return self.__angle_a
+        return self._angle_a
 
     @property
     def angle_v(self) -> float | None:
         """Returns the angle relative to the true vertical axis (global Z axis)."""
-        return self.__angle_v
+        return self._angle_v
 
     @property
     def grid(self) -> str | None:
         """Returns the grid name to which the completion data applies."""
-        return self.__grid
+        return self._grid
 
     @property
     def depth_to_top(self) -> float | None:
         """Returns the depth to the top of the completion interval as a float."""
-        return self.__depth_to_top
+        return self._depth_to_top
 
     @property
     def depth_to_bottom(self) -> float | None:
         """Returns the depth to the bottom as a float."""
-        return self.__depth_to_bottom
+        return self._depth_to_bottom
 
     @property
     def perm_thickness_ovr(self) -> float | None:
         """Returns premeability thickness override value as a float to use for the completion interval."""
-        return self.__perm_thickness_ovr
+        return self._perm_thickness_ovr
 
     @property
     def dfactor(self) -> float | None:
         """Returns the non-darcy factor to use for rate dependent skin calculations as a float."""
-        return self.__dfactor
+        return self._dfactor
 
     @property
     def rel_perm_method(self) -> int | None:
         """Writes the relative permeability used for completion as an int."""
-        return self.__rel_perm_method
+        return self._rel_perm_method
 
     @property
     def status(self) -> str | None:
         """Returns the status of the layer as a string whether it is 'ON' or 'OF'."""
-        return self.__status
+        return self._status
 
     @property
     def peaceman_well_block_radius(self) -> Optional[float]:
         """Writes the pressure equivalent radius of the grid block."""
-        return self.__peaceman_well_block_radius
+        return self._peaceman_well_block_radius
 
     @property
     def perm_thickness_mult(self) -> Optional[float]:
         """Returns the permeability thickness multiplier to use for the completion interval."""
-        return self.__perm_thickness_mult
+        return self._perm_thickness_mult
 
     @property
     def date_format(self) -> Optional[DateFormat]:
