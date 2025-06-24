420ee33c8f418d46668129c37defb7a1fd051e8c
Jonny Hall <121860375+jonnyh404@users.noreply.github.com>
2025-06-24 16:10:22 +0100

    adds more tests for grid functions and fixes several underlying bugs (#477)

--- ResSimpy/Nexus/DataModels/StructuredGrid/NexusGridArrayFunction.py
+++ ResSimpy/Nexus/DataModels/StructuredGrid/NexusGridArrayFunction.py
@@ -23,42 +23,47 @@ class NexusGridArrayFunction(GridArrayFunction):
     def to_string(self) -> str:
         """Creates a string representation of the object."""
 
-        # keep a running string
-        func_string = """"""
+        # compile the function string
+        func_string = "FUNCTION"
 
-        # create beginning of string by checking region type which can have three values
-        if self.region_number is not None and self.region_type in ['IREGION', 'ITRAN']:
+        # create beginning of string by checking region type
+        if self.region_number is not None and self.region_type is not None:
             # the region number immediately follows the region type
-            reg_num_str = ' '.join(repr(num) for num in self.region_number)
-            func_string = f"""FUNCTION {self.region_type}
-""" + reg_num_str + '\n'
-        elif self.region_type in [None]:
-            # function keyword needed even though region type not specified
-            func_string = """FUNCTION
-"""
+            reg_num_str = ' '.join(str(num) for num in self.region_number)
+            func_string += f" {self.region_type}\n{reg_num_str}\n"
+        elif self.region_type is None:
+            # if there is no region type, we just write FUNCTION
+            func_string += "\n"
+
         if self.blocks is not None:
             # after region type comes BLOCKS
-            block_num_str = ' '.join(repr(num) for num in self.blocks)
-            func_string = """FUNCTION
-BLOCKS""" + ' ' + block_num_str + '\n'
+            block_num_str = ' '.join(str(num) for num in self.blocks)
+            func_string += f"BLOCKS {block_num_str}\n"
 
         if self.grid_name is not None:
             # after blocks comes the grid
             func_string += f'GRID {self.grid_name}\n'
 
+        if self.input_range is not None:
+            input_vals = (
+                ' '.join(f'{i:.5e}' if len(str(i)) > 5 else str(i) for pair in self.input_range for i in pair))
+            func_string += 'RANGE INPUT' + ' ' + input_vals + '\n'
+
         if self.output_range is not None:
-
             output_vals = (
-                ' '.join(f'{i:.1e}' if len(str(i)) > 5 else repr(i) for pair in self.output_range for i in pair))
-
+                ' '.join(f'{i:.5e}' if len(str(i)) > 5 else str(i) for pair in self.output_range for i in pair))
             func_string += 'RANGE OUTPUT' + ' ' + output_vals + '\n'
 
-        if self.function_type is not None and self.function_values is not None and self.function_type.value is not None:
+        if self.drange is not None:
+            drange_vals = ' '.join(f'{num:.5e}' if len(str(num)) > 5 else str(num) for num in self.drange)
+            func_string += 'DRANGE' + ' ' + drange_vals + '\n'
 
+        if self.function_type is not None and self.function_type.value is not None:
             # ANALYT must always precede function type
-
-            func_vals = ' '.join(repr(num) for num in self.function_values)
-            func_string += f'ANALYT {self.function_type.value} ' + func_vals + '\n'
+            func_vals = ''
+            if self.function_values is not None:
+                func_vals = ' '+' '.join(str(num) for num in self.function_values)
+            func_string += f'ANALYT {self.function_type.value}' + func_vals + '\n'
 
         # specify the end of the string with output/input arrays
         input_arr = output_arr = ''
