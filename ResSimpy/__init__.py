0e54a74a814becf2427f09d472e803e8b01b568a
Jonny Hall <121860375+jonnyh404@users.noreply.github.com>
2026-06-08 14:34:20 +0100

    adds module level imports for some useful objects (#523)

--- ResSimpy/__init__.py
+++ ResSimpy/__init__.py
@@ -16,7 +16,16 @@ __version__ = "0.0.0"  # Set at build time
 from ResSimpy.Nexus.NexusSimulator import NexusSimulator
 from ResSimpy.OpenGoSim.OpenGoSimSimulator import OpenGoSimSimulator
 
+# Useful regularly used objects
+from ResSimpy.Time.ISODateTime import ISODateTime
+from ResSimpy.Enums.UnitsEnum import UnitSystem
+from ResSimpy.Nexus.NexusEnums.DateFormatEnum import DateFormat
+
+
 __all__ = [
     "NexusSimulator",
     "OpenGoSimSimulator",
+    "ISODateTime",
+    "UnitSystem",
+    "DateFormat",
    ]
---
+++ tests/test_module_level_imports.py
@@ -0,0 +1,15 @@
+def test_import_ISODateTime():
+    from ResSimpy import ISODateTime
+    assert ISODateTime is not None
+
+def test_import_UnitSystem():
+    from ResSimpy import UnitSystem
+    assert UnitSystem is not None
+
+def test_import_DateFormat():
+    from ResSimpy import DateFormat
+    assert DateFormat is not None
+
+def test_import_NexusSimulator():
+    from ResSimpy import NexusSimulator
+    assert NexusSimulator is not None
