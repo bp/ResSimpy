16bf17a409bf423ae52b14d662151c8d76e71e35
MahmoudAlamar03 <hamadaalzain88@gmail.com>
2026-07-23 12:45:57 -0500

    testing when values are missing

--- tests/Nexus/test_nexus_tolpv.py
+++ tests/Nexus/test_nexus_tolpv.py
@@ -198,3 +198,33 @@ TOLPV ALL
     assert simulation.grid.tolpv_grid_name == 'ALL'
 
 
+def test_load_tolpv_warns_when_value_missing(mocker):
+    fcs_file_contents = f"RUNCONTROL /run_control/path\nDATEFORMAT DD/MM/YYYY\nSTRUCTURED_GRID test_structured_grid.dat"
+    structured_grid_name = os.path.join('testpath1', 'test_structured_grid.dat')
+    structured_grid_file_contents = """
+    NX NY NZ
+    10 10 3
+TOLPV
+"""
+
+    def mock_open_wrapper(filename, mode):
+        mock_open = mock_multiple_files(mocker, filename, potential_file_dict=
+        {'testpath1/nexus_run.fcs': fcs_file_contents,
+         '/run_control/path': '',
+         structured_grid_name: structured_grid_file_contents,
+         }).return_value
+        return mock_open
+
+    mocker.patch("builtins.open", mock_open_wrapper)
+    mocker.patch("os.path.isfile", lambda x: True)
+    mocker.patch("os.path.exists", lambda x: True)
+
+    simulation = NexusSimulator(origin='testpath1/nexus_run.fcs')
+    assert simulation.grid is not None
+    with pytest.warns(UserWarning, match=r'Unable to parse TOLPV line: TOLPV'):
+        result = simulation.grid.tolpv
+
+    assert result is None
+    assert simulation.grid.tolpv_grid_name is None
+
+
